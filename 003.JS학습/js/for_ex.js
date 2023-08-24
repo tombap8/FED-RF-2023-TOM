ea30015590b2cd8f44e1757e27d2df06dd5e595b
Tom B. Parker <tombap8@hotmail.com>
2023-08-24 10:19:32 +0900

    ss

--- 003.JS학습/js/for_ex.js
+++ 003.JS학습/js/for_ex.js
@@ -7,12 +7,12 @@
 // 1. 대상선정 ////////////
 
 // 1-1. 이벤트대상 : 미니언즈
-var mini = document.querySelectorAll('.mini');
+var mini = document.querySelectorAll(".mini");
 
-// 1-2. 출력대상 : 파란박스
-var blue_box = document.querySelector('.Bcase');
+// 1-2. 출력대상 : 파란박스안 미니언즈 박스
+var blue_box = document.querySelector(".mini-space");
 
-console.log('미니언즈:',mini,blue_box);
+console.log("미니언즈:", mini, blue_box);
 
 // 2. 이벤트 연결하기 ///////////////////
 
@@ -22,14 +22,13 @@ console.log('미니언즈:',mini,blue_box);
 // 기본for문으로 반복하여 개수만큼 이벤트 셋팅하기
 // for(시;한;증){코드}
 // (0부터 시작; 개수보다 작을때까지 반복; 1씩증가)
-for(var i=0;i<mini.length;i++){
-    mini[i].addEventListener('click',insertMini);
-    console.log('for문내i:',i);
+
+for (var i = 0; i < mini.length; i++) {
+    mini[i].addEventListener("click", insertMini);
+    console.log("for문내i:", i);
 } //////////// for ///////////////
 
-console.log('for문밖i:',i);
-
-
+console.log("for문밖i:", i);
 
 // 3. 함수만들기 ////////////////////////
 
@@ -39,13 +38,13 @@ console.log('for문밖i:',i);
     함수명 : insertMini
     기능 : 미니언즈 이미지 개수만큼 넣기
 ****************************************/
-function insertMini(){
+function insertMini() {
     // 1. 호출확인
     // console.log('미니넣어!',this);
     // this 나자신! -> 호출한 .mini
 
     // 2. 셋팅된 개수 속성(data-cnt) 가져오기
-    var cnt = this.getAttribute('data-cnt');
+    var cnt = this.getAttribute("data-cnt");
     // getAttribute(속성명) -> 속성값 가져오기! 내장함수
     // console.log('data-cnt:',cnt);
 
@@ -53,12 +52,10 @@ function insertMini(){
     // 대상: .Bcase -> blue_box
     // for(시;한;증){코드}
     // (0부터; 개수보다작을때까지; 1씩증가)
-    for(var j=0; j<cnt; j++){
+    for (var j = 0; j < cnt; j++) {
         blue_box.innerHTML += `
         <img src="./images/Minions.png" alt="미니언즈">
         `;
     } ///////// for ////////////
-
-
 } //////// insertMini함수 ///////////////
 /////////////////////////////////////////
