387d61c69fcbd4ae435f4c443a5cc5e52e30205d
Tom B. Parker <tombap8@hotmail.com>
2023-09-01 14:59:44 +0900

    ss

---
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -0,0 +1,15 @@
+// 도깨비 PJ 메인 JS - main.js
+
+// 로딩구역 호출설정
+window.addEventListener('DOMContentLoaded',loadFn);
+
+//// 로딩구역 함수 /////////////////
+function loadFn(){
+    // 로딩확인
+    console.log('로딩완료!');
+
+    // 부드러운 스크롤 적용
+    startSS();
+
+} ////////// loadFn함수 //////////////
+//////////////////////////////////////
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/smoothScroll23.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/smoothScroll23.js
@@ -6,9 +6,9 @@
 // startSS()함수를 호출하여 사용
 function startSS() {
     // 전체 스크롤 대상일때 document를 보냄
-    // new SmoothScroll(document, 60, 12)
+    new SmoothScroll(document, 30, 22)
     // 특정박스일 경우 document.querySelector(선택요소)를 씀!
-    new SmoothScroll(document.querySelector('.wrap'), 30, 22)
+    // new SmoothScroll(document.querySelector('.wrap'), 60, 12)
 }
 
 // 전역변수 스크롤 위치값
