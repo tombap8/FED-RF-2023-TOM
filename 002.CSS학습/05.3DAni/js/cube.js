74f8177843bb15e56d1519d23396c05128dab023
Tom B. Parker <tombap8@hotmail.com>
2023-09-01 11:11:11 +0900

    ss

--- 002.CSS학습/05.3DAni/js/cube.js
+++ 002.CSS학습/05.3DAni/js/cube.js
@@ -10,3 +10,23 @@
     버튼으로 변경되어 있음!
 
 *************************************/
+
+// window 로드 이벤트 호출 ///
+window.addEventListener('DOMContentLoaded',loadFn);
+
+// DOM 선택함수 //////
+const qs = x => document.querySelector(x);
+const qsa = x => document.querySelectorAll(x);
+
+/////// 로딩함수 //////////////
+function loadFn(){
+    // 함수호출확인
+    console.log('로딩완료!');
+
+    // 1. 대상선정: .btngo
+    const btngo = qs('.btngo');
+    console.log('대상:',btngo);
+
+
+} /////////////// loadFn함수 //////////////
+///////////////////////////////////////////
