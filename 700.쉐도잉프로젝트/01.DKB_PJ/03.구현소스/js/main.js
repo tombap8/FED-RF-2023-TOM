3dd6335d2e0fab304e0fbf0fe2d4205688f9bfb8
Tom B. Parker <tombap8@hotmail.com>
2023-09-01 16:20:42 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -11,5 +11,21 @@ function loadFn(){
     // 부드러운 스크롤 적용
     startSS();
 
+    // 부드러운 스크롤 때문에 마우스휠 이벤트막기가
+    // 작동되어 캐릭터설명박스 작은 스크롤도 작동안됨!
+    // 따라서 이벤트 버블링을 막아줘야함!
+    // event.stopPropagation()
+    // 이벤트 객체의 이벤트 버블링 막아주는 메서드임! 
+    
+    // 대상: .desc-box
+    let desc_box = document.querySelectorAll('.desc-box');
+    console.log(desc_box);
+
+    // 모든 캐릭터 설명박스는 이벤트 버블링 막기!!!
+    // -> 여기서 마우스휠 됨!!!
+    desc_box.forEach(ele=>{ // ele - 요소자신
+        ele.onwheel = e => e.stopPropagation();
+    });
+
 } ////////// loadFn함수 //////////////
 //////////////////////////////////////
