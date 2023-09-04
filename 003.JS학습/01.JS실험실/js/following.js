f28bc416a97576de263de300835cc3251fc08e3a
Tom B. Parker <tombap8@hotmail.com>
2023-09-04 11:21:25 +0900

    ss

--- 003.JS학습/01.JS실험실/js/following.js
+++ 003.JS학습/01.JS실험실/js/following.js
@@ -13,6 +13,7 @@ function loadFn(){
     // 함수호출확인
     console.log('로딩완료!');
 
+    // [1] html 코드 넣기 ///////
     // 1. 대상선정
     // 변경대상: .cont-box
     const cont_box = qs('.cont-box');
@@ -36,6 +37,60 @@ function loadFn(){
 
     // 3. 대상에 html넣기
     cont_box.innerHTML = hcode;
+
+
+    // [2] 따라다니는 원 셋팅하기
+    // 1. 대상선정 : 
+    // (1) 움직일 대상 : .mover
+    const mover = qs('.mover');
+    // (2) 이벤트 대상 : document.body
+    const myBody = document.body;
+
+    // console.log('요소:',mover,myBody);
+
+    /* 
+        ★[[ 이벤트발생시 위치값 ]]★
+
+        1. clientX, clientY
+            -> 현재 보이는 브라우저 화면이 기준
+            -> 화면을 기준한 fixed 포지션에서 주로 사용!
+
+        2. offsetX, offsetY
+            -> 이벤트 대상이 기준
+            -> 특정박스이 부모자격박스로 부터 위치를 사용할 경우
+
+        3. pageX, pageY
+            -> 전체 문서를 기준(스크롤 화면을 포함)
+            -> 화면을 기준한 absolute 포지션에서 주로 사용!
+
+        4. screenX, screenY
+            -> 모니터 화면을 기준
+    */
+
+    // 무버 크기의 절반계산
+    let gap = mover.clientWidth/2;
+    // 선택요소의 크기 JS
+    // width는 clientWidth, height는 clientHeight
+    console.log('무버width:',gap);
+
+    // 2. 이벤트 대상에 마우스 무드 이벤트가 적용될때
+    // 무버가 따라다니게 기능 구현
+    myBody.onmousemove = e => { // e - 이벤트 전달변수
+        // 1.위치값 가져오기(박스중앙위치로 보정)
+        let posx = e.pageX - gap;
+        let posy = e.pageY - gap;
+
+        // 2.무버에 위치값 적용하기
+        mover.style.top = posy + 'px';
+        mover.style.left = posx + 'px';
+
+
+        // console.log('pageX:',e.pageX,'/pageY:',e.pageY);
+        // console.log('screenX:',e.screenX,'/screenY:',e.screenY);
+        // console.log('offsetX:',e.offsetX,'/offsetY:',e.offsetY);
+        // console.log('clientX:',e.clientX,'/clientY:',e.clientY);
+
+    }; ///////// onmousemove //////////
 
 
 
