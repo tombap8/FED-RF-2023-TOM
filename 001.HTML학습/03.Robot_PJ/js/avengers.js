8bf5eda98b427e4d27a9a78905cd1356c16eb07c
Tom B. Parker <tombap8@hotmail.com>
2023-09-05 10:24:20 +0900

    ss

--- 001.HTML학습/03.Robot_PJ/js/avengers.js
+++ 001.HTML학습/03.Robot_PJ/js/avengers.js
@@ -4,6 +4,8 @@
 const qs = x => document.querySelector(x);
 const qsa = x => document.querySelectorAll(x);
 
+// 새로고침시 맨위로 이동
+setTimeout(()=>{window.scrollTo(0,0)},500);
 
 window.addEventListener('wheel',(e)=>{
     // 기본기능 막기 : preventDefault()
@@ -17,6 +19,18 @@ window.addEventListener('wheel',(e)=>{
     // window.innerHeight*(e.wheelDelta<0?1:0)
     // 윈도우높이값 * 음수명 1곱하고 양수면 0곱함
     // 아랫방향은 윈도우높이값만큼 가고 윗방향은 위치값 0임!
+
+    // 두번째 페이지일때 동영상 플레이하기
+    if(e.wheelDelta<0){ // 아래로 내려갈때 - 자동플레이
+        qs('.trailer-box iframe').src=
+        'https://www.youtube.com/embed/Ko2NWhXI9e8?autoplay=1';
+    } ///// if ////////////
+    else{ // 위로 올라올때 - 멈춤
+        qs('.trailer-box iframe').src=
+        'https://www.youtube.com/embed/Ko2NWhXI9e8';
+    } ////// else ///////////////
+
+
 },{passive:false});
 
 // passive:false 설정값 변경을 해야
