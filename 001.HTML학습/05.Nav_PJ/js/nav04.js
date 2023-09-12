c3259bd1cde8658dd63a8d133ed7df4fb1fdc931
Tom B. Parker <tombap8@hotmail.com>
2023-09-12 11:06:59 +0900

    Update nav04.js

--- 001.HTML학습/05.Nav_PJ/js/nav04.js
+++ 001.HTML학습/05.Nav_PJ/js/nav04.js
@@ -31,7 +31,42 @@ const domFn = {
 
   // 4. 함수 생성하기
   function showSub(){
-    console.log('나야나!',this)
+    // this는 클릭된 li요소 자신!
+    console.log('나야나!',this);
+
+    // 1. 서브메뉴 유무 판별하기!
+    // -> .smenu가 하위에 있는지 여부를 알아내기
+    let isSub = domFn.qsEl(this,'.smenu');
+    console.log('서브결과:',isSub);
+
+    // 조건문 조건식에 null값은 false처리됨!
+    if(isSub){ // .smenu가 있는 경우만 들어옴!
+        // 2. 서브메뉴 내부 ol박스 높이값 읽기
+        let hv = domFn.qsEl(isSub,'ol').clientHeight;
+        console.log('처리해봐!',hv);
+
+        // 3. .smenu(isSub)의 높이값 적용하기
+        isSub.style.height = hv + 'px';
+
+        // 4. 클릭된 li 이외의 li 서브는 닫기
+        // 방법: 전체 li를 순회하며 현재 li만 빼고 처리
+        // 사용할 DOM메서드 : 요소.isSameNode(비교요소)
+        // -> 같은 요소면 true / 다른요소면 false값 리턴
+        gnbList.forEach(ele=>{
+            // 판별결과
+            let result = ele.isSameNode(this)
+            console.log("같니?",result);
+            // 결과처리: false일때만 높이값 0처리
+            // false일때 treu처리하려면 논리부정(!)사용            
+
+            if(!result){ // false일때만 들어옴!
+                if(domFn.qsEl(ele,'.smenu'))
+                domFn.qsEl(ele,'.smenu').style.height = '0px';
+            } /////// if ////////
+        }); ///////// forEach ///////
+
+        
+    } ////// if /////////
   } ////////// showSub 함수 ////////
 
 
