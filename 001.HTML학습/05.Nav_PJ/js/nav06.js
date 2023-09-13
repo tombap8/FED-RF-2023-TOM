20275e3c9df90aceb16057b9f9d5f43693b8c086
Tom B. Parker <tombap8@hotmail.com>
2023-09-13 16:22:24 +0900

    Update nav06.js

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -21,4 +21,46 @@ const domFn = {
 const gnbBox = domFn.qs('.gnb');
 console.log('대상',gnbBox);
 
+
+// 3. 객체 데이터로 html 코드만들기
+let hcode = '';
+
+for(let x in mdata){
+  console.log('속성명:',x); 
+  hcode +=
+  `  
+  <ul>
+  <li>
+      <a href="">${x}</a>
+      <div class="smenu">
+          <aside class="smbx">
+              <h2>
+                  <div class="stit">2차</div>
+                  <a href="">전체보기</a>
+                  <div class="swrap">
+                      <dl>
+                          <dt></dt>
+                          <dd><a href="">요기</a></dd>
+                      </dl>
+                  </div>
+              </h2>
+          </aside>
+      </div>
+  </li>
+</ul>
+
+
+
+  `
+} //////// for in /////////////
+
+// 확인
+console.log('코드:',hcode);
+
+
+
+
+
 // ul>li>a{1차}+.smenu>aside.smbx>h2>(.stit{2차}+a{전체보기})+.swrap>dl>dt+dd>a{요기}
+
+
