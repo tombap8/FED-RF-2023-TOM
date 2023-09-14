0786145ce853356cdab8aeee09fb9cdaeac4e57e
Tom B. Parker <tombap8@hotmail.com>
2023-09-14 13:11:51 +0900

    Update nav06.js

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -122,7 +122,6 @@ gnbBox.innerHTML = hcode;
 *************************************/
 // 1. 대상선정
 const gnb = domFn.qsa('.gnb>ul>li');
-console.log('대상:',gnb);
 
 // 2. 이벤트 설정하기
 // 이벤트 종류: mouseover / mouseout
@@ -133,12 +132,17 @@ gnb.forEach(ele=>{
 
 // 3.함수만들기
 function overFn(){
-  console.log('나야나',this);
-
+  // console.log('오버',this);
+  // 1.하위 .smbx 높이값 알아오기
+  let hv = domFn.qsEl(this,'.smbx').clientHeight;
+  // console.log('높이:',hv);
+  // 2.하위 서브메뉴박스 만큼 .smenu 높이값 주기
+  domFn.qsEl(this,'.smenu').style.height = hv + 'px';
 } //////////// overFn 함수 ////////////
 
 function outFn(){
-  console.log('나야나',this);
-
+  // console.log('아웃',this);
+  // 서브메뉴 박스 높이값 0만들기!
+  domFn.qsEl(this,'.smenu').style.height = '0px';
 } //////////// outFn 함수 ////////////
 
