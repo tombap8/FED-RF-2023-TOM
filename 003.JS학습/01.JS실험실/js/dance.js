0bee01c9cc0f638ccb1827de9e4b84bfa5a104e9
Tom B. Parker <tombap8@hotmail.com>
2023-09-14 16:06:32 +0900

    Update dance.js

--- 003.JS학습/01.JS실험실/js/dance.js
+++ 003.JS학습/01.JS실험실/js/dance.js
@@ -21,7 +21,7 @@ const domFn = {
   // 2. 대상선정: .slidePg
   const slidePg = domFn.qs('.slidePg');
 
-  console.log('대상:',slidePg);
+//   console.log('대상:',slidePg);
 
   // 3. 코드만들기
   let hcode = '';
@@ -47,14 +47,59 @@ const domFn = {
 
   // 이벤트 대상: window
   // 이벤트 종류: scroll
-  // 위치대상: .slidePg -> slidePg변수
+  // 위치대상: .tpg -> 스티키박스를 싸고있는 부모박스
+  const tpg = domFn.qs('.tpg');
+  // 움직일대상: .slidePg>ul
+  const target = domFn.qs('.slidePg>ul');
+
+  // 윈도우 이벤트 걸기
   domFn.addEvt(window,'scroll',moveSlide);
 
   function moveSlide(){
-    // 스크롤 위치값
-    let scTop = window.scrollY;
+    // 스크롤 위치값 -> 불필요!
+    // let scTop = window.scrollY;
 
-    console.log('나야나!',domFn.getBCR(slidePg),scTop);
+    // 1. 스티키 부모박스 바운딩top값
+    let bTop =  domFn.getBCR(tpg);
+    // console.log('나야나!',bTop);
+
+    // 2. 바운딩값으로  대상 left위치 변경하기
+    // 움직일대상: 스티키박스 -> .slidePg>ul
+    // 움직이기시작은 바운딩값이 0이하일때부터!!!
+    if(bTop<=0 && bTop >= -3000){
+        target.style.left = bTop + 'px';
+    }
+    // 윗쪽(0초과)일때 처음위치 재설정하기
+    else if(bTop > 0){
+        target.style.left = '0px';
+    }
 
 
   } //////////// moveSlide 함수 //////////////
+
+
+  ///////////////////////////////////////////////
+  // 추가 기능 : 메뉴에 오버시 움직이는 배경 구현하기
+
+  // 1.대상선정
+  // 이벤트 대상: .gnb li
+  // 변경 대상: .mbg
+  const gnbList = domFn.qsa('.gnb li');
+  const mbg = domFn.qs('.mbg');
+//   console.log('대상:',gnbList,mbg);
+
+// 2.이벤트 설정하기
+// 이벤트종류: mouseover / mouseout
+gnbList.forEach(ele=>{
+    domFn.addEvt(ele,'mouseover',overFn);
+    domFn.addEvt(ele,'mouseout',outFn);
+}); ///////// forEach ///////////////
+
+// 3. 함수만들기 /////////////////////
+function overFn(){
+    console.log('오버:',this);
+} ///////// overFn 함수 ///////////
+function outFn(){
+    console.log('아웃:',this);
+} ///////// outFn 함수 ///////////
+
