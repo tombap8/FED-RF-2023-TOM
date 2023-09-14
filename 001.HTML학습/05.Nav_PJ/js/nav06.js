a6f968230d153bcc3ddead4193cfa7f1dd385fad
Tom B. Parker <tombap8@hotmail.com>
2023-09-14 11:13:33 +0900

    Update nav06.js

--- 001.HTML학습/05.Nav_PJ/js/nav06.js
+++ 001.HTML학습/05.Nav_PJ/js/nav06.js
@@ -48,7 +48,7 @@ for(let x in mdata){ // x는 속성명
 } //////// for in /////////////
 
 // 확인
-console.log('코드:',hcode);
+// console.log('코드:',hcode);
 
 
 // 내부 for in문 코드 생성함수 만들기 /////
@@ -78,6 +78,10 @@ function makeCode(obj){ // obj - 객체 전달값
 
 } //////////// makeCode 함수 ///////////////
 
+// 최종 GNB출력하기 ////////////
+gnbBox.innerHTML = hcode;
+
+
 
 /***************************************** 
   [ 배열데이터를 변경하여 다시 배열로 만들기 : map() ]
@@ -87,6 +91,7 @@ function makeCode(obj){ // obj - 객체 전달값
   -> 새로 만들어진 배열데이터를 현재 자리에 그대로 
   출력하고자 할때 배열메서드 join(구분자)를 이용하여
   배열을 하나의 문자형 데이터로 만들주면 편리하다!
+
   예) const aa = ['하하','호호']
   `<div>${aa.map(val=>`<h2>${val}</h2>`).join('')}</div>`
   - 결과 : `<div><h2>하하</h2><h2>호호</h2></div>`
@@ -98,12 +103,12 @@ function makeCode(obj){ // obj - 객체 전달값
   const bb = aa.map(val=>val+'씨');
   -> 결과: ['신숙씨','상호씨','경미씨']
 *****************************************/
-const aa = ['신숙','상호','경미'];
-console.log(aa);
-const bb = aa.map(val=>val+'씨');
-console.log(bb);
-const cc = bb.map((val,idx)=>`<div>${idx+'.'+val}</div>`)
-console.log(cc,'배열이니? ',Array.isArray(cc));
+// const aa = ['신숙','상호','경미'];
+// console.log(aa);
+// const bb = aa.map(val=>val+'씨');
+// console.log(bb);
+// const cc = bb.map((val,idx)=>`<div>${idx+'.'+val}</div>`)
+// console.log(cc,'배열이니? ',Array.isArray(cc));
 
 
 // ul>li>a[href='#']{1차}+.smenu>aside.smbx>h2>(.stit{1차}+a[href='#']{전체보기})+.swrap>dl>dt{2차}+dd>a[href='#']{3차}
