613524d16518ee3c532801f961b0114ccfd8e1c4
Tom B. Parker <tombap8@hotmail.com>
2023-09-18 13:01:39 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/data_drama.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/data_drama.js
@@ -28,12 +28,12 @@ const liveData = [
     4.날짜 : date
 *************************************/
 const posterData = [
-  { idx: "1", title: "도깨비 5인 단체 포스터", imgName: "poster1", date: "2023-09-18" },
-  { idx: "2", title: "공유, 김고은 커플 포스터", imgName: "poster2", date: "2023-09-18" },
+  { idx: "1", title: "도깨비 5인 단체 포스터", imgName: "poster5", date: "2023-09-18" },
+  { idx: "2", title: "공유, 김고은 커플 포스터", imgName: "poster4", date: "2023-09-18" },
   { idx: "3", title: "이동욱, 유인나 커플 포스터", imgName: "poster3", date: "2023-09-18" },
-  { idx: "4", title: "공유, 이동욱 포스터", imgName: "poster4", date: "2023-09-18" },
-  { idx: "5", title: "공유, 이동욱, 육성재 포스터", imgName: "poster5", date: "2023-09-18" },
-]; /////////// liveData 배열 /////////
+  { idx: "4", title: "공유, 이동욱 포스터", imgName: "poster2", date: "2023-09-18" },
+  { idx: "5", title: "공유, 이동욱, 육성재 포스터", imgName: "poster1", date: "2023-09-18" },
+]; /////////// posterData 배열 /////////
 
 
 // 3. GNB 하위메뉴 구성 데이터
