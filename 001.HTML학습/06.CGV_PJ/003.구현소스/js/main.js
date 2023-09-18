a015655fcc5e3465631207550e26670de8f8aab6
Tom B. Parker <tombap8@hotmail.com>
2023-09-18 12:43:42 +0900

    ss

--- 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
+++ 001.HTML학습/06.CGV_PJ/003.구현소스/js/main.js
@@ -87,10 +87,16 @@ console.log('대상:',btnMap,gmap,cbtn);
 // 2. 이벤트설정 및 함수구현
 // 2-1. 맵버튼 클릭시
 domFn.addEvt(btnMap,'click',()=>{
+  /* 구글맵보이기 */
   gmap.classList.add('on');
+  /* 가림막보이기 */
+  document.body.classList.add('on');
 }); //////// click이벤트함수 ////////
 
 // 2-2. 닫기버튼 클릭시
 domFn.addEvt(cbtn,'click',()=>{
+  /* 구글맵숨기기 */
   gmap.classList.remove('on');
+  /* 가림막숨기기 */
+  document.body.classList.remove('on');
 }); //////// click이벤트함수 ////////
