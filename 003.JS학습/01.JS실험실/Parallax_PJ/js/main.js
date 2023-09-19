5e220365181bca8dcb2d3588d29312d8f41dc768
Tom B. Parker <tombap8@hotmail.com>
2023-09-19 13:12:58 +0900

    Update main.js

--- 003.JS학습/01.JS실험실/Parallax_PJ/js/main.js
+++ 003.JS학습/01.JS실험실/Parallax_PJ/js/main.js
@@ -1,7 +1,7 @@
 // 패럴렉스 PJ JS - main.js
 
 // 부드러운 스크롤 불러오기
-import {startSS,setPos} from "./smoothScroll20.js";
+import { startSS, setPos } from "./smoothScroll20.js";
 // DOM함수 불러오기
 import dFn from "./dom.js";
 
@@ -30,80 +30,76 @@ startSS();
 
 // 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
 setTimeout(() => {
-    // 윈도우 스크롤 맨위로!
-    window.scrollTo(0,0);
-    // 부드러운 스크롤 위치값 반영!
-    setPos(0);
-    // 안하면 원래 위치로 스크롤시 튐!
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤시 튐!
 }, 400);
 // 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
-dFn.addEvt(window,'mouseup',()=>setPos(window.scrollY)); 
+dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
 //////// mouseup /////////////
 
 // 0. 키보드 방향키 이동시 위치값 반영
-dFn.addEvt(window,'keyup',()=>setPos(window.scrollY)); 
+dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
 //////// mouseup /////////////
 
 // 1. 대상선정 ///////////////
 // 1-1. 글자박스
-const txtBox = dFn.qsa('.txt');
+const txtBox = dFn.qsa(".txt");
 // 1-2. 아이콘박스
-const icon = dFn.qsa('.icon');
+const icon = dFn.qsa(".icon");
 
 // console.log(txtBox,icon);
 
 // 2. 이벤트 설정하기 ////////////
 // 대상: window / 이벤트종류: scroll
-dFn.addEvt(window,'scroll',scrollFn);
+dFn.addEvt(window, "scroll", scrollFn);
 
 // 3. 함수 만들기 ////////////////
 // 3-1. 스크롤 이벤트 함수 ////
-function scrollFn(){
-    // console.log('스크롤~~~!');
+function scrollFn() {
+  // console.log('스크롤~~~!');
 
-    // 1. 대상1 : 글자박스 패럴렉스 호출!
-    txtBox.forEach(ele=>
-        moveEl(dFn.getBCR(ele),ele,setH2));
+  // 1. 대상1 : 글자박스 패럴렉스 호출!
+  txtBox.forEach((ele) => moveEl(dFn.getBCR(ele), ele, setH2));
 
-    // 2. 대상2 : 아이콘 패럴렉스 호출!
-    icon.forEach(ele=>
-        moveEl(dFn.getBCR(ele),ele,setH1));
-
+  // 2. 대상2 : 아이콘 패럴렉스 호출!
+  icon.forEach((ele) => moveEl(dFn.getBCR(ele), ele, setH1));
 } /////////// scrollFn 함수 ////////
 
 // 셋팅값 변수 ////
 // 윈도우 높이값
 const winH = window.innerHeight;
 // 패럴렉스 범위변수
-const setH1 = 100, setH2 = 200;
+const setH1 = 100,
+  setH2 = 200;
 
 // 3-2. 패럴렉스 이동함수 /////
-function moveEl(elPos,ele,setH){
-    // 전달변수 : 
-    // (1) elPos - 위치값(getBCL값)
-    // (2) ele - 대상요소(패럴렉스대상)
-    // (3) setH - 움직일범위수(클수록 빠르게 이동)
-    // console.log(
-    //     '위치:',elPos,
-    //     '\n대상:',ele,
-    //     '\n범위:',setH);
+function moveEl(elPos, ele, setH) {
+  // 전달변수 :
+  // (1) elPos - 위치값(getBCL값)
+  // (2) ele - 대상요소(패럴렉스대상)
+  // (3) setH - 움직일범위수(클수록 빠르게 이동)
+  // console.log(
+  //     '위치:',elPos,
+  //     '\n대상:',ele,
+  //     '\n범위:',setH);
 
-    // [ 패럴렉스 범위 : 윈도우 높이값 ~ 0 ]
-    // 화면에서 완전히 사라질때 범위는 0보다 작다(약간의 마이너스값)
-    if(elPos < winH && elPos > -200){
-        // 1. 위치이동값 계산
-        // 실제이동값 = 정한범위 - (위치값*정한범위 / 전체범위)
-        let x = setH - (elPos*setH / winH);
-        console.log('x:',-x);
+  // [ 패럴렉스 범위 : 윈도우 높이값 ~ 0 ]
+  // 화면에서 완전히 사라질때 범위는 0보다 작다(약간의 마이너스값)
+  if (elPos < winH && elPos > -200) {
+    // 1. 위치이동값 계산
+    // 실제이동값 = 정한범위 - (위치값*정한범위 / 전체범위)
+    let x = setH - (elPos * setH) / winH;
+    console.log("x:", -x);
 
-        // 2. 해당요소의 위치값 이동 CSS반영하기
-        // Y축이동시 윗쪽방향은 마이너스임! -x
-        ele.style.transform = 
-            `translateY(${-x}px)`;
-    } ///////////// if //////////////////
-    
+    // 2. 해당요소의 위치값 이동 CSS반영하기
+    // Y축이동시 윗쪽방향은 마이너스임! -x
+    ele.style.transform = `translateY(${-x}px)`;
+  } ///////////// if //////////////////
 
-    /***************************** 
+  /***************************** 
         [ 패럴렉스 위치계산 ]
         1. 전체범위 : 윈도우높이값
         2. 위치값 : getBoundingClientRect().top
@@ -123,5 +119,3 @@ function moveEl(elPos,ele,setH){
 
     *****************************/
 } //////////// moveEl 함수 //////////
-
-
