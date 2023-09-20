f9d2f133386297e59199e87fbb660d8f70c5a6f8
Tom B. Parker <tombap8@hotmail.com>
2023-09-20 13:17:40 +0900

    ss

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -11,6 +11,22 @@ import { gridData, gnbData } from "./data_drama.js";
 // 부드러운 스크롤 적용 //////////
 startSS();
 
+// 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
+setTimeout(() => {
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤시 튐!
+}, 400);
+// 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
+dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
+//////// mouseup /////////////
+
+// 0. 키보드 방향키 이동시 위치값 반영
+dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
+//////// mouseup /////////////
+
 // 부드러운 스크롤 때문에 마우스휠 이벤트막기가
 // 작동되어 캐릭터설명박스 작은 스크롤도 작동안됨!
 // 따라서 이벤트 버블링을 막아줘야함!
