0fdc2c19b7dd716360a70d7020b04089f5ee019d
Tom B. Parker <tombap8@hotmail.com>
2023-09-27 17:44:23 +0900

    Update 01.시작하기.jsx

--- 004.React학습/react-cdn/js/01.시작하기.jsx
+++ 004.React학습/react-cdn/js/01.시작하기.jsx
@@ -14,30 +14,27 @@
 
 // 생성자 함수를 만들어서 사용한다!
 // 대문자로 시작하는 이름을 사용한다!
-function MyFirstReact(){
-    return(
-        <div>
-            <h1>잘해보자! 리액트!!!</h1>
-            <h2>
-            [ 리액트란? ] <br /><br />
-                # 프론트엔드 JS 라이브러리다!
-                # 사용자 UI의
-                구성요소를 빌드하기 위한도구 <br />
-                [ 작동원리 ] <br /><br />
-                # 가상돔(Virtual
-                DOM)을 사용하여 최소의 html리소스를 사용함으로 <br />
-                빠르고 쉽게 UI화면의 구성한다! <br />
-                # 가상돔은 실제DOM을 변경하기전에
-                메모리상에서 구성하는 가짜 DOM이다! <br />
-                # 변경사항을 한번에 구성하여
-                반영하기 위한도구다! <br />
-                # 리액트는 변경하고자 하는 부분만 업데이트
-                가능함! <br />
-            </h2>
-        </div>
-    );
+function MyFirstReact() {
+  return (
+    <div>
+      <h1>잘해보자! 리액트!!!</h1>
+      <h2>
+        [ 리액트란? ] <br />
+        <br />
+        # 프론트엔드 JS 라이브러리다! # 사용자 UI의 구성요소를 빌드하기 위한도구{" "}
+        <br />
+        [ 작동원리 ] <br />
+        <br />
+        # 가상돔(Virtual DOM)을 사용하여 최소의 html리소스를 사용함으로 <br />
+        빠르고 쉽게 UI화면의 구성한다! <br />
+        # 가상돔은 실제DOM을 변경하기전에 메모리상에서 구성하는 가짜 DOM이다!{" "}
+        <br />
+        # 변경사항을 한번에 구성하여 반영하기 위한도구다! <br />
+        # 리액트는 변경하고자 하는 부분만 업데이트 가능함! <br />
+      </h2>
+    </div>
+  );
 } ///////// MyFirstReact 생성자함수 ///////////
-
 
 // 리액트 html대상요소에 삽입하기
 /* 
@@ -55,5 +52,4 @@ function MyFirstReact(){
     document.querySelector("#root")
 */
 
-ReactDOM.render(<MyFirstReact />,
-document.querySelector('#mydiv'));
+ReactDOM.render(<MyFirstReact />, document.querySelector("#mydiv"));
