fbdf34370f64a282c243c51c25a3cca7cabcf64c
Tom B. Parker <tombap8@hotmail.com>
2023-10-05 16:14:41 +0900

    Update js4-4.array_sort.js

--- 003.JS학습/js/js4-4.array_sort.js
+++ 003.JS학습/js/js4-4.array_sort.js
@@ -43,6 +43,56 @@ import dFn from './dom.js';
     [-> 기준정렬 : 내림차순]
     배열변수.reverse() -> 기본정렬
 
+    //////////////////////////////////////////////
+
+    ->>> 숫자형, 문자형에 무관하게 하나로 처리하기!!!
+     [ sort() 메서드만 사용하여 정렬잡기 ]
+
+    (비교함수사용)
+    배열변수.sort(function(x,y){
+        if(x>y) return 1;
+        if(x<y) return -1;
+        return 0;
+    })
+    
+    -> 단순화하기(삼항연산자사용!)
+
+    배열변수.sort(function(x,y){
+        return x == y ? 0 : x > y ? 1 : -1;
+    })
+
+    -> 더 줄이기! (화살표함수 사용!)
+
+    배열변수.sort((x,y)=> x == y ? 0 : x > y ? 1 : -1)
+
+
+    -> 리턴값의 의미(오름차순)
+    1) if(x>y) return 1; -> x가 y뒤에 옴 (리턴값 양수)
+    2) if(x<y) return -1; -> x가 y앞에 옴 (리턴값 음수)
+    3) return 0; -> x,y 정렬을 유지 (리턴값 0)
+
+    -> 내림차순은 양수/음수만 반대로 써주면 된다!
+
+    [ 정렬시 데이터 유의사항 ]
+    1. 문자형일 경우 대소문자가 섞여있으면 대문자나 소문자중
+    하나로 통일하여 비교해야함(toUpperCase()/toLowerCase())
+    예)
+    배열변수.sort((x,y)=>{
+        let a = x.toUpperCase(),
+            b = y.toUpperCase();
+        
+        return a == b ? 0 : a > b ? 1 : -1;
+    })
+
+    2. 날짜정렬도 숫자와 동일함
+    (날짜데이터 자체가 숫자형으로 되어있음)
+
+    3. 특정언어의 특수문자일 경우 
+    localeCompare() 메서드로 문자열 비교를 한다!
+    
+    예) 특수문자 x변수를 y변수와 변환후 비교 
+        x.localeCompare(y)
+
 
 ******************************************************/
 
@@ -88,13 +138,21 @@ dFn.addEvt(selBox,'change',function(){
     let optVal = this.value;
     console.log('숫자정렬변경:',optVal);
     // 2. 정렬 분기 : 1 - 오름차순 / 2 - 내림차순
-    if(optVal==1){ // 오름차순
+    if(optVal==1){ // 오름차순        
+        // [sort()메서드 내부함수사용법]
+        // a > b 일때 true이면 1처리 -> 순서바꿈!
+        arrNumber.sort((a,b)=>a==b?0:a>b?1:-1);
+
         // sort() 빼기연산처리 : 앞수-뒷수
-        arrNumber.sort((a,b)=>a-b);
+        // arrNumber.sort((a,b)=>a-b);
     } /////// if ///
-    else if(optVal==2){ // 내림차순
+    else if(optVal==2){ // 내림차순        
+        // [sort()메서드 내부함수사용법]
+        // a > b 일때 true이면 -1처리 -> 순서안바꿈!
+        arrNumber.sort((a,b)=>a==b?0:a>b?-1:1);
+
         // sort() 빼기연산처리 : 뒷수-앞수
-        arrNumber.sort((a,b)=>b-a);
+        // arrNumber.sort((a,b)=>b-a);
     } ///// else if ////
 
     // 화면출력 
@@ -129,10 +187,20 @@ dFn.addEvt(selBox2,'change',function(){
     console.log('숫자정렬변경:',optVal);
     // 2. 정렬 분기 : 1 - 오름차순 / 2 - 내림차순
     if(optVal==1){ // 오름차순
-        arrString.sort();
+        // [sort()메서드 내부함수사용법]
+        // a > b 일때 true이면 1처리 -> 순서바꿈!
+        arrString.sort((a,b)=>a==b?0:a>b?1:-1);
+
+        // 오름차순 기본 메서드 sort() 사용
+        // arrString.sort();
     } /////// if ///
     else if(optVal==2){ // 내림차순
-        arrString.reverse();
+        // [sort()메서드 내부함수사용법]
+        // a > b 일때 true이면 -1처리 -> 순서안바꿈!
+        arrString.sort((a,b)=>a==b?0:a>b?-1:1);
+
+        // 내림차순 기본 메서드 reverse() 사용
+        // arrString.reverse();
     } ///// else if ////
 
     // 화면출력 
