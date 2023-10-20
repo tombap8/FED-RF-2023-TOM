7bb03702cb9eedda5d85de6829c0d96de5739dbf
Tom B. Parker <tombap8@hotmail.com>
2023-10-20 17:17:45 +0900

    ss

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -2,6 +2,8 @@
 import data from "./data.js";
 import myData from "./data.js";
 import myData2 from "./data2.js";
+// JS 기능함수 
+import { initFn,firstOneFn } from "./act_effect.js";
 
 // 두개의 데이터를 배열로 구성
 const twoData = [myData, myData2];
@@ -20,9 +22,76 @@ function MainComponent() {
   // 데이터가 0번째 즉, 첫번째 배열순번 데이터를 불러올
   // 순번값을 셋팅함!
   const [dataNum,setDataNum] = React.useState(0);
+  
+  // 테스트 후크상태변수
+  const [test,setTest] = React.useState(0);
 
   console.log('최초값:',dataNum);
 
+  // 가상돔에서 실제돔에 반영후 DOM에 구현할
+  // JS코드는 어디에 넣어야 하는가?
+  // ->>> useEffect()
+  // ->>> useLayoutEffect()
+
+  console.log('컴포넌트 그냥구역:',
+    document.querySelector('.img-box'));
+
+    ////////////////////////////////////////////////
+    // [ 리액트 컴포넌트 렌더링 후 실행함수 호출하기 ]
+    ///////////////////////////////////////////////////
+
+    // [ 1. 컴포넌트가 뿌려지기 애니메이션 적용하기 ]
+    React.useLayoutEffect(initFn);
+
+    // [ 2. 처음 한번만 타이틀 글자 커졌다가 작아지기 ]
+    React.useEffect(firstOneFn,[])
+    ////////////////////////////////////////////////////
+
+
+    
+    // [ useEffect 테스트 코드 ] //////////    
+    // 순수 useEffect
+    // -> 매번 업데이트 시에도 실행함
+    React.useEffect(()=>{
+      console.log('useEffect 순수');
+      // console.log('useEffect순수 구역 JS:',
+      //   document.querySelector('.img-box'));
+      // console.log('useEffect순수 구역 제이쿼리:',
+      //   $('.img-box'));
+      
+    });
+
+    // 빈 배열옵션 useEffect ///////
+    // -> 페이지로딩후 단 한번만 실행함!
+    React.useEffect(()=>{
+      console.log('useEffect 빈 배열옵션');
+    },[]);
+      
+    // 의존성 배열옵션 useEffect
+    // -> 페이지로딩후 단 한번만 실행함!
+    React.useEffect(()=>{
+      console.log('useEffect 의존성 배열옵션 test');
+    },[test]);
+
+    React.useEffect(()=>{
+      console.log('useEffect 의존성 배열옵션 dataNum');
+    },[dataNum]);
+    // 의존성이 다수일 경우 [] 배열형태의 옵션에
+    // 콤마로 연결하여 등록해준다!
+      
+      // 랜더링 후 화면출력전 상태
+    React.useLayoutEffect(()=>{
+      console.log('useLayoutEffect 구역 JS:');
+        // 버튼을 display:none
+        // $('.btn-gong').hide();
+  });
+
+  // 의존성 테스트 함수 /////
+  const testFn = () => {
+    setTest(test?0:1);
+    console.log('test 후크변수 변경!',test);
+
+  }; //////// testFn함수 ////////////
 
   // 데이터 변경호출 함수 //////////
   const chgData = () => {
@@ -32,6 +101,8 @@ function MainComponent() {
     console.log('업데이트값:',dataNum);
   }; //////// chgData함수 ///////////
 
+  // 최종 리턴 코드 /////////
+  // 함수, 변수, 구현소스는 모두 return위쪽에 코딩!
   return (
     <React.Fragment>
       {/* 1. 타이틀 */}
@@ -43,15 +114,18 @@ function MainComponent() {
         dataNum?'효진은 오늘도 쨍~합니다!':
         '공유는 오늘도 멋찝니다!'}</h2>
         {/* 이미지 */}
-        <img src={dataNum?
-        "./images/gallery/hyo.jpg":
-          "./images/vans/gongyoo.jpg"} 
-          alt={dataNum?"엘레강스한 효진":"멋진공유"} />
+        <div className="img-box">
+          <img src={dataNum?
+          "./images/gallery/hyo.jpg":
+            "./images/vans/gongyoo.jpg"} 
+            alt={dataNum?"엘레강스한 효진":"멋진공유"} />
+        </div>
       </section>
       {/* 3. 데이터 변경 버튼 */}
       <button onClick={chgData} className="btn-gong">
         {dataNum?'공유':'효진'}초이스 바로가기
       </button>
+      <button onClick={testFn} >의존성테스트</button>
       {/* 4. 상품리스트박스 */}
       <div className="gwrap">
         <GoodsCode idx={dataNum} />
@@ -82,6 +156,7 @@ function GoodsCode(props) { // idx - 데이터 배열순번
     </ol>
   ));
 } /////////// GoodsCode //////////////////
+
 
 // 메인컴포넌트 출력하기 //////////
 ReactDOM.render(<MainComponent />, 
