311dcda0472d3f661c32b95fe719dcfea1990d28
Tom B. Parker <tombap8@hotmail.com>
2023-10-20 13:15:22 +0900

    ss

--- 004.React학습/react-ex/js/01.공유신발.jsx
+++ 004.React학습/react-ex/js/01.공유신발.jsx
@@ -2,12 +2,22 @@
 import myData from "./data.js";
 import myData2 from "./data2.js";
 
-console.log('여성의류Data:',myData2);
+// 두개의 데이터를 배열로 구성
+const twoData = [myData, myData2];
+
+// console.log('데이터:',twoData);
 
 // 메인 컴포넌트 /////////////////
 // 메인의 의미는? 다른 구성요소 컴포넌트들을 모아
 // 최종적으로 랜더링하는 구성 컴포넌트를 말한다!
 function MainComponent() {
+
+
+  // 데이터 변경호출 함수 //////////
+  const chgData = () => {
+    console.log('바꿔~!');
+  }; //////// chgData함수 ///////////
+
   return (
     <React.Fragment>
       {/* 1. 타이틀 */}
@@ -18,7 +28,11 @@ function MainComponent() {
         {/* 이미지 */}
         <img src="./images/vans/gongyoo.jpg" alt="멋진공유" />
       </section>
-      {/* 3. 상품리스트박스 */}
+      {/* 3. 데이터 변경 버튼 */}
+      <button onClick={chgData} className="btn-gong">
+        효진초이스 바로가기
+      </button>
+      {/* 4. 상품리스트박스 */}
       <div className="gwrap">
         <GoodsCode />
       </div>
