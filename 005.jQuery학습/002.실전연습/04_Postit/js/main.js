dbe37446f80718b4f4a39bd116b533095d5a5957
Tom B. Parker <tombap8@hotmail.com>
2023-10-23 10:40:43 +0900

    Update main.js

--- 005.jQuery학습/002.실전연습/04_Postit/js/main.js
+++ 005.jQuery학습/002.실전연습/04_Postit/js/main.js
@@ -7,7 +7,16 @@ $(()=>{
     const dgEle = $('.draggable');
 
     // 2. 드래그 기능 설정하기 : draggable() 메서드호출
-    dgEle.draggable();
+    // dgEle.draggable() -> 호출만 해도 드래그기능은 됨!
+    // 드래그 기능 옵션은 {} 객체로 전달함
+    dgEle.draggable({
+        // 커서모양 '쥔주먹'표시
+        cursor: 'grabbing', 
+        // 드래그 대상 상위처리(z-index)
+        stack: '.draggable', 
+        // 이동중 투명도 설정
+        opacity: 0.7
+    });
 
 
 
