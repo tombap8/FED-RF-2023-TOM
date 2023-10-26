5a3c55aa3528fa0de1a3c28998df5c5ce1a1dbdd
Tom B. Parker <tombap8@hotmail.com>
2023-10-26 16:29:21 +0900

    ss

---
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/gallery.js
@@ -0,0 +1,24 @@
+// 보그 PJ 갤러리 페이지 JS - gallery.js
+
+var swiper = new Swiper(".mySwiper", {
+    autoplay: {
+        // 자동넘김 시간
+        delay: 2500,
+        // 상호작용(건들여놓기)에 대한 재가동 없애기 안씀(false)
+        disableOnInteraction: false,
+    },
+    // 한번에 보일 슬라이드 수
+    slidesPerView: 3,
+    // 사이간격
+    spaceBetween: 20,
+    // 하단블릿
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    // 양쪽이동버튼
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
