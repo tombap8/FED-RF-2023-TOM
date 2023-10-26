c718f0a66d3c5fb85e2b9af042f3d6bcf33f4c17
Tom B. Parker <tombap8@hotmail.com>
2023-10-26 11:42:02 +0900

    Update main.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/main.js
@@ -3,14 +3,6 @@
 import dFn from "./dom.js";
 
 
-// 부드러운 스크롤 모듈
-import { startSS, setPos } from "./smoothScroll23.js";
-
-
-// [1] 부드러운 스크롤 적용 //////////
-startSS();
-
-
 // [1] 메인 페이지 등장액션 클래스 넣기
 // 대상: .main-area section
 const hideBox = $('.main-area section');
@@ -41,29 +33,8 @@ hideBox.each((idx,ele)=>{
 let winH = $(window).height()/3*2;
 console.log('윈도우 높이값:',winH);
 
-// 스크롤 메뉴 적용대상: #top-area
-const topArea = $('#top-area');
-
-// 탑버튼 : .tbtn
-const tbtn = $('.tbtn');
-
-$(window).scroll(()=>{
-    // 세로방향 스크롤 위치값
-    let scTop = $(window).scrollTop();
-    console.log('스크롤~~~~!',scTop);
-
-    // 1.스크롤 위치값이 100을 초과하면 
-    //  슬림 상단 클래스넣기
-    if(scTop>100) topArea.addClass('on');
-    else topArea.removeClass('on');
-
-    // 2. 맨위로 이동버튼 300초과시 보이고
-    //     300미만일때 숨기기
-    // 대상: .tbtn
-    if(scTop>300) tbtn.addClass('on');
-    else tbtn.removeClass('on');
-    
-    // 3.등장액션 클래스 적용하기
+$(window).scroll(()=>{    
+    // 등장액션 클래스 적용하기 //////
     hideBox.each((idx,ele)=>{
         if(idx!=0){
             let val = dFn.getBCR(ele);
@@ -77,12 +48,3 @@ $(window).scroll(()=>{
 
 }); ///////// scroll /////////////
 
-// 맨위로 버튼 클릭시 맨위로 가기 //
-// 부드러운 스크롤 사용하므로 그쪽함수를 이용함!
-tbtn.click((e)=>{
-    // 1.a요소 기본이동막기
-    e.preventDefault();
-    // 2.부드러운 스크롤 위치값 변경(0으로)
-    setPos(0);
-    console.log('나클릭!');
-}); /////////// click //////////
