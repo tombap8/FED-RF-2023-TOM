5ad8148fdd0d3938ec9b3dcfe375e8fb87871604
Tom B. Parker <tombap8@hotmail.com>
2023-10-30 10:40:45 +0900

    Update icecream.js

--- 005.jQuery학습/002.실전연습/05_DragGallery/js/icecream.js
+++ 005.jQuery학습/002.실전연습/05_DragGallery/js/icecream.js
@@ -14,14 +14,29 @@ $(()=>{ ////////// jQB /////////////////
     const target = $('#move');
 
 
-    // 2. 변경내용 : 제이쿼리 UI 드래그 설정하기
+    // 2. 변경내용
+    // 2-1. 제이쿼리 UI 드래그 설정하기
     target.draggable({
         axis:'x' // x축고정
-    })
-    // 타겟에 트랜지션 설정
+    }) ///// draggable //////
+    // 2-2. 타겟에 트랜지션 설정
     .css({
         transition: TRS_TIME_DT
-    })
+    }); ///////// css ///////
+
+    // 2-3. 한계값 설정하기 /////////////////
+
+    // 2-3.1. 화면크기 업데이트 함수
+    const updataWin = () => $(window).width();
+    // 2-3-2. 최초 윈도우가로크기 업데이트
+    let winW = updataWin();
+    // 2-3-3. 윈도우 리사이즈시 윈도우가로크기 업데이트
+    $(window).resize(()=>{
+        winW = updataWin();
+        console.log('업데이트 화면가로크기:',winW);
+    }); ////// resize //////////
+    
+    console.log('처음화면가로크기:',winW);
 
 
 
