976bf532b7589235488ebd255be9810fef81237e
Tom B. Parker <tombap8@hotmail.com>
2023-10-30 14:58:40 +0900

    Update member.js

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/03.구현코드/js/member.js
@@ -1,3 +1,21 @@
 // 보그 PJ 회원가입 페이지 JS - member.js
 
+/********************************************** 
+    [ 사용자 입력폼 유효성 검사 ]
+    - 이벤트 종류 : blur(포커스가 빠질때 발생)
+    - 제이쿼리 이벤트 메서드 :  blur()
+    - 이벤트 대상: 
+    ->입력요소 중 text(아이디email2제외),password
+        form.logF input[type=text][id!=email2],
+        form.logF input[type=password],
+    -> 요소뒤 대괄호는 속성선택자(CSS원래문법)
+    [id!=email2] !=은 같지않다(제이쿼리용문법)
 
+**********************************************/
+$(`form.logF input[type=text][id!=email2],
+form.logF input[type=password]`)
+// .on('blur',function(){})
+.blur(function(){
+    console.log('나야나!',this);
+
+}); ///////////////// blur 메서드 /////////////////
