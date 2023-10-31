9438033e3777ce40a3999661250c3fec3bc8f27c
Tom B. Parker <tombap8@hotmail.com>
2023-10-31 17:21:15 +0900

    Update list.js

--- 005.jQuery학습/003.응용구현/jq_board/js/list.js
+++ 005.jQuery학습/003.응용구현/jq_board/js/list.js
@@ -16,18 +16,75 @@ Number(a.idx)==Number(b.idx)?
 // 대상: #board tbody
 const board = $('#board tbody');
 
+// 리스트 번호변수
+let listNum = 0;
+
+// 숫자1씩증가 함수
+const addNum = () => ++listNum;
+
+// console.log('증가수:',addNum());
+
+
+// 페이징 되는 리스트 만들기 //////////////
+// 페이징의 기본 원리:
+// 1. 한페이지당 리스트수를 정하여 전체 페이지 개수 구하기
+// 2. 개수만큼 리스트에 데이터를 반복문으로 생성함
+// 3. 페이지 번호에 따라 시작번호를 업데이트 한다 
+
+// 페이징 관련 변수들 /////
+// [1] 한페이지당 리스트수 : pgBlock
+const pgBlock = 9;
+// [2] 페이지 순번 : pgNum -> 증감예정!
+let pgNum = 6;
+// [3] 전체 레코드 수 : totalCnt
+const totalCnt = bData.length;
+// [4] 페이징 블록 계산하기
+let pagingBlock = Math.floor(totalCnt/pgBlock);
+// [5] 나머지 리스트 여부 : 0이면 다음 페이지 없음!
+let addOver = totalCnt % pgBlock;
+
+// 시작번호 업데이트
+listNum = (pgNum-1)*pgBlock;
+
+let hcode = '';
+// 리스트 블록으로 리스트 소스 만들기
+for(let i = (pgNum-1)*pgBlock;i<pgBlock*pgNum;i++){
+    // i가 전체 개수보다 같거나 크면 break (for문 나가기!)
+    console.log(i);
+    if(i >= totalCnt) break;
+
+    hcode +=
+        `
+            <tr>
+                <td>${addNum()}</td>
+                <td>${bData[i].tit}</td>
+                <td>${bData[i].writer}</td>
+                <td>${bData[i].date}</td>
+                <td>${bData[i].cnt}</td>
+            </tr>
+        `;
+} //////// for //////////
+
+// 코드 반영하기 ///
+board.html(hcode);
+
+console.log(`pgBlock:${pgBlock
+}\npgNum:${pgNum}\ntotalCnt:${totalCnt
+}\npagingBlock:${pagingBlock}\naddOver:${addOver}`);
+
+
 // 데이터 태그 생성후 태그넣기
-board.html(
-    bData.map(v=>`
-        <tr>
-            <td>${v.idx}</td>
-            <td>${v.tit}</td>
-            <td>${v.writer}</td>
-            <td>${v.date}</td>
-            <td>${v.cnt}</td>
-        </tr>
-    `).join('')
-); ///////// html //////////
+// board.html(
+//     bData.map(v=>`
+//         <tr>
+//             <td>${addNum()}</td>
+//             <td>${v.tit}</td>
+//             <td>${v.writer}</td>
+//             <td>${v.date}</td>
+//             <td>${v.cnt}</td>
+//         </tr>
+//     `).join('')
+// ); ///////// html //////////
 
 // 데이터를 태그로 구성함
 // 태그 구조: <tr><td></td>...</tr>
