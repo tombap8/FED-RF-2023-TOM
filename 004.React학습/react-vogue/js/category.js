2838ebb16599ad3483afc306183fccf76a38a62a
Tom B. Parker <tombap8@hotmail.com>
2023-11-06 12:47:00 +0900

    Update category.js

--- 004.React학습/react-vogue/js/category.js
+++ 004.React학습/react-vogue/js/category.js
@@ -215,46 +215,55 @@ function ItemList(props){
   // cname - 카테고리명(클래스명넣기)
   // tit - 리스트 타이틀
 
+  // 태그처리 구분 코드생성 함수
+  const makeCode = data => {
+    console.log('배열인가?',Array.isArray(data));
+    // 배열데이터인 경우 ////
+    if(Array.isArray(data)){ 
+        return( 
+          <h2>
+              <small>{data[0]}</small><br/>{data[1]}
+          </h2>
+        )
+    } /////// if ////////
+    else{ // 배열이 아닌경우 ////
+      return(<h2>{data}</h2>);
+    } /////// else ///////
+  }; ////////// makeCode함수 ////////////
+
   return(
     // 2-2. 카테고리 페이지 컨텐츠영역
     // html출력일 경우 dangerouslySetInnerHTML을 사용함!
     // <요소 dangerouslySetInnerHTML={{__html:값}}>
     // <h2 dangerouslySetInnerHTML={{__html:props.tit[0]}}>
+    // -> script태그와 같은 위험요소가 실행될 수 있으므로
+    // 데이터에서 직접적으로 태그를 넣는 방법을 비추천함!
+    // -> 개선: 태그 데이터를 제거하고 데이터화 함!
+    // -> runway 데이터를 배열형으로 만들고 첫번째 데이터를
+    // small태그로 싸고 뒤에 br태그로 줄바꿈한 구성을 
+    // 컴포넌트에서 구성하도록 변경함!
 
     <div className={"cat-cont-area "+props.cname}>
       <section className="pt2">
         <div className="cbx bgi bg1-1">
-          {console.log(Array.isArray(props.tit[0]))}
-            { // 배열데이터면 small태그 처리함
-              Array.isArray(props.tit[0]) &&
-              <h2>
-                  <small>{props.tit[0][0]}</small><br/>
-                  {props.tit[0][1]}
-              </h2>
-              }
-              { // 배열데이터가 아니면 일반처리함
-                !Array.isArray(props.tit[0]) &&
-                <h2>
-                  {props.tit[0]}
-                </h2>
-              }
+            {makeCode(props.tit[0])}
         </div>
         <div className="cbx bgi bg1-2">
-          <h2>{props.tit[1]}</h2>
+            {makeCode(props.tit[1])}
         </div>
         <div className="cbx bgi bg1-3">
-          <h2>{props.tit[2]}</h2>
+            {makeCode(props.tit[2])}
         </div>
       </section>
       <section className="pt2">
         <div className="cbx bgi bg2-1">
-          <h2>{props.tit[3]}</h2>
+            {makeCode(props.tit[3])}
         </div>
         <div className="cbx bgi bg2-2">
-          <h2>{props.tit[4]}</h2>
+            {makeCode(props.tit[4])}
         </div>
         <div className="cbx bgi bg2-3">
-          <h2>{props.tit[5]}</h2>
+            {makeCode(props.tit[5])}
         </div>
       </section>
     </div>
