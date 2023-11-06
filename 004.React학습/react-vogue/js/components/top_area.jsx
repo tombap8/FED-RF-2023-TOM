c0140bc897cadb607cbfe386f7662a5568721609
Tom B. Parker <tombap8@hotmail.com>
2023-11-06 15:20:32 +0900

    Update top_area.jsx

--- 004.React학습/react-vogue/js/components/top_area.jsx
+++ 004.React학습/react-vogue/js/components/top_area.jsx
@@ -8,98 +8,117 @@ import { makeLink } from "./linksys2.js";
   기능 : 상단영역 메뉴, 로그 등 요소 구성
 *******************************************/
 export default function TopArea() {
-    // 컴포넌트 요소 랜더링 직전 호출구역
-    // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더랭)
-    React.useLayoutEffect(makeLink);
-  
-    // GNB용 메뉴 배열변수
-    const gnbText = [
-      "FASHION",
-      "BEAUTY",
-      "LIVING",
-      "PEOPLE",
-      "VIDEO",
-      "RUNWAY",
-      "TIME & GEM",
-      "SHOPPING",
-    ];
-  
-    // 메뉴 클릭시 변수 변경함수
-    const chgCat = (data) => {
-      console.log("나야나!", data);
-    }; ///////////// chgCat 함수 ///////////
-  
-    return (
-      <React.Fragment>
-        {/* 1-1.상단메뉴 */}
-        <div className="tmenu">
-          {/* 1-1-1.sns박스 */}
-          <div className="sns">
-            <a href="#" className="fi fi-instagram">
-              <span className="ir">인스타그램</span>
-            </a>
-            <a href="#" className="fi fi-facebook">
-              <span className="ir">페이스북</span>
-            </a>
-            <a href="#" className="fi fi-twitter">
-              <span className="ir">트위터</span>
-            </a>
-            <a href="#" className="fi fi-youtube-play">
-              <span className="ir">유튜브</span>
-            </a>
-            <a href="#" className="fi cas">
-              <span className="ir">카카오스토리</span>
-            </a>
-          </div>
-          {/* 1-1-2.사이드메뉴 */}
-          <div className="sideMenu">
-            <ul className="smbx">
-              <li>
-                <a href="#">SIDE MENU</a>
-                {/* 서브메뉴 */}
-                <ol className="smsub">
-                  <li>
-                    <a href="#">회사 소개</a>
-                  </li>
-                  <li>
-                    <a href="#">광고 및 제휴</a>
-                  </li>
-                  <li>
-                    <a href="#">개인정보 처리방침</a>
-                  </li>
-                </ol>
-              </li>
-              <li>
-                <a href="#">SUBSCRIBE</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        {/* 1-2.로고박스 */}
-        <h1 className="logo">
-          <a href="#">
-            <img src="./images/mlogo.png" alt="메인로고" />
+  // 컴포넌트 요소 랜더링 직전 호출구역
+  // -> 컴포넌트는 모두 만들어진 후 화면뿌리기 직전(가랜더랭)
+  React.useLayoutEffect(makeLink);
+
+  // GNB용 메뉴 배열변수
+  const gnbText = [
+    "FASHION",
+    "BEAUTY",
+    "LIVING",
+    "PEOPLE",
+    "VIDEO",
+    "RUNWAY",
+    "TIME & GEM",
+    "SHOPPING",
+  ];
+
+  // 메뉴 클릭시 변수 변경함수
+  const chgCat = (data) => {
+    console.log("나야나!", data);
+  }; ///////////// chgCat 함수 ///////////
+
+  return (
+    <React.Fragment>
+      {/* 1-1.상단메뉴 */}
+      <div className="tmenu">
+        {/* 1-1-1.sns박스 */}
+        <div className="sns">
+          <a href="#" className="fi fi-instagram">
+            <span className="ir">인스타그램</span>
           </a>
-        </h1>
-        {/* 1-3.GNB박스 */}
-        <nav className="gnb">
-          <ul>
-            {gnbText.map((v) => (
-              <li>
-                <a href="#" onClick={() => chgCat(v)}>
-                  {v}
-                </a>
-              </li>
-            ))}
-  
+          <a href="#" className="fi fi-facebook">
+            <span className="ir">페이스북</span>
+          </a>
+          <a href="#" className="fi fi-twitter">
+            <span className="ir">트위터</span>
+          </a>
+          <a href="#" className="fi fi-youtube-play">
+            <span className="ir">유튜브</span>
+          </a>
+          <a href="#" className="fi cas">
+            <span className="ir">카카오스토리</span>
+          </a>
+        </div>
+        {/* 1-1-2.사이드메뉴 */}
+        <div className="sideMenu">
+          <ul className="smbx">
             <li>
-              {/* 돋보기 검색버튼 */}
-              <i href="#" className="fi fi-search">
-                <span className="ir">search</span>
-              </i>
+              <a href="#">SIDE MENU</a>
+              {/* 서브메뉴 */}
+              <ol className="smsub">
+                <li>
+                  <a href="#">회사 소개</a>
+                </li>
+                <li>
+                  <a href="#">광고 및 제휴</a>
+                </li>
+                <li>
+                  <a href="#">개인정보 처리방침</a>
+                </li>
+              </ol>
+            </li>
+            <li>
+              <a href="#">SUBSCRIBE</a>
             </li>
           </ul>
-        </nav>
-      </React.Fragment>
-    );
-  } //////////// TopArea 컴포넌트 /////////////
+        </div>
+      </div>
+      {/* 1-2.로고박스 */}
+      <h1 className="logo">
+        <a href="#">
+          <img src="./images/mlogo.png" alt="메인로고" />
+        </a>
+      </h1>
+      {/* 1-3.GNB박스 */}
+      <nav className="gnb">
+        <ul>
+          {gnbText.map((v) => (
+            <li>
+              <a href="#" onClick={() => chgCat(v)}>
+                {v}
+              </a>
+            </li>
+          ))}
+
+          <li>
+            {/* 돋보기 검색버튼 */}
+            <i href="#" className="fi fi-search">
+              <span className="ir">search</span>
+            </i>
+          </li>
+        </ul>
+      </nav>
+    </React.Fragment>
+  );
+} //////////// TopArea 컴포넌트 /////////////
+
+/********************************************
+ * 컴포넌트명 : MobBtns
+ * 기능 : 모바일용 버튼 생성 컴포넌트
+ ********************************************/
+function MobBtns() {
+  return (
+    <React.Fragment>
+      {/* 모바일용 햄버거버튼 */}
+      <a href="#" className="mobtn hbtn fi fi-nav-icon">
+        <span className="ir">GNB button</span>
+      </a>
+      {/* 모바일용 검색버튼 */}
+      <a href="#" className="mobtn sbtn fi fi-search">
+        <span className="ir">search</span>
+      </a>
+    </React.Fragment>
+  );
+} //////////////// ModBtns 컴포넌트 /////////////
