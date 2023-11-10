26af2a9474be590394488c899449245bc8038063
Tom B. Parker <tombap8@hotmail.com>
2023-11-10 12:13:49 +0900

    Update Banner.jsx

--- 004.React학습/react-app/tom-app/my-first-react-app/src/dc/contents/Banner.jsx
+++ 004.React학습/react-app/tom-app/my-first-react-app/src/dc/contents/Banner.jsx
@@ -13,15 +13,24 @@ import "jquery-ui-dist/jquery-ui";
 /// 슬라이드 기능 구현 함수 ///////////
 function slideFn() {
   // 1. 대상선정
+  // (1) 슬라이드
   const sldBox = $(".slider");
+  // (6) 슬라이드 블릿
+  const indic = $('.indic li');
+  // console.log('블릿:',indic);
 
   // 2. 변수설정
-  // 애니시간
+  // (1) 애니시간
   const A_TM = 600;
-  // 애니이징
+  // (2) 애니이징
   const A_ES = "easeInOutQuint";
-  // 광클상태변수(1-불허용,0-허용)
+  // (3) 광클상태변수(1-불허용,0-허용)
   let cSts = 0;
+  // (4) 슬라이드순번
+  let sNum = 0;
+  // (5) 슬라이드개수
+  const sCnt = sldBox.find('li').length;
+  // console.log('슬라이드개수:',sCnt);
 
   // 3. 이벤트설정 및 기능구현
   // 이동버튼 클릭시
@@ -48,6 +57,9 @@ function slideFn() {
         // 동시에 left값은 0으로 초기화함!
         .css({left:'0'});
       });
+      // 슬라이드 순번 증가(끝번호보다 크면 0)
+      sNum++;
+      if(sNum>=sCnt) sNum=0;
     } //////// if /////////
     // 2-2. 왼쪽버튼
     else{
@@ -58,19 +70,31 @@ function slideFn() {
       // left값을 0으로 애니메이션
       .animate({left:'0'},A_TM,A_ES);
 
+      // 슬라이드순번 감소(0보다 작으면 끝번호)
+      sNum--;
+      if(sNum<0) sNum=sCnt-1;
     } /////// else /////////
+
+    // console.log('슬순번:',sNum);
+
+    // 블릿해당순번 클래스'on'넣기(다른li는 제거)
+    indic.eq(sNum).addClass('on')
+    .siblings().removeClass('on');
   }); /////////// click //////////
 } //////////// slideFn 함수 ///////////
 
 // 배너 컴포넌트 //
 export function Banner(props) {
   // category - 카테고리 분류명(베너 데이터선택)
+  
+  // 선택데이터
+  const selData = banData[props.category];
 
   // 페이지 랜더링후 실행구역
   useEffect(() => {
     console.log("랜더링후~!");
-    // 슬라이드 기능구현함수 호출
-    slideFn();
+    // 슬라이드 기능구현함수 호출 : 선택데이터가 1초과일때
+    if(selData.length>1) slideFn();
   }); ///////// useEffect //////////
 
   // 리스트만들기 함수
@@ -82,9 +106,6 @@ export function Banner(props) {
       </li>
     ));
   };
-
-  // 선택데이터
-  const selData = banData[props.category];
 
   // 코드리턴 ////////////////
   return (
