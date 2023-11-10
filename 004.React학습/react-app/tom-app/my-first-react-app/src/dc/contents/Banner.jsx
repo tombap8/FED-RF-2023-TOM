17e548bd3f7e963550056d09f8f787e0c525d09a
Tom B. Parker <tombap8@hotmail.com>
2023-11-10 11:19:36 +0900

    Update Banner.jsx

--- 004.React학습/react-app/tom-app/my-first-react-app/src/dc/contents/Banner.jsx
+++ 004.React학습/react-app/tom-app/my-first-react-app/src/dc/contents/Banner.jsx
@@ -6,15 +6,48 @@ import { banData } from "../data/banner";
 // 배너CSS
 import "../../css/banner.css";
 import { useEffect } from "react";
+// 제이쿼리 + 제이쿼리UI
+import $ from "jquery";
+import "jquery-ui-dist/jquery-ui";
+
+/// 슬라이드 기능 구현 함수 ///////////
+function slideFn() {
+  // 1. 대상선정
+  const sldBox = $(".slider");
+
+  // 2. 이벤트설정 및 기능구현
+  // 이동버튼 클릭시
+  $(".abtn").click(function () {
+    // 1. 오른쪽버튼 여부
+    let isR = $(this).is(".rb");
+    console.log("버튼클릭!", isR);
+
+    // 2. 버튼별분기
+    // 2-1. 오른쪽버튼
+    if (isR) {
+      // 슬라이드가 왼쪽으로 이동함
+      // left값이 -100%
+      sldBox.animate({ left: "-100%" }, 600, "easeInOutQuint", 
+      () => { // 콜백함수(애니후)
+        // 맨앞li 맨뒤로 이동
+        sldBox.append(sldBox.find('li').first())
+        // 동시에 left값은 0으로 초기화함!
+        .css({left:'0'});
+      });
+    } //////// if /////////
+  }); /////////// click //////////
+} //////////// slideFn 함수 ///////////
 
 // 배너 컴포넌트 //
 export function Banner(props) {
   // category - 카테고리 분류명(베너 데이터선택)
 
   // 페이지 랜더링후 실행구역
-  useEffect(()=>{
-    console.log('랜더링후~!');
-  });
+  useEffect(() => {
+    console.log("랜더링후~!");
+    // 슬라이드 기능구현함수 호출
+    slideFn();
+  }); ///////// useEffect //////////
 
   // 리스트만들기 함수
   const makeList = (data) => {
@@ -33,27 +66,26 @@ export function Banner(props) {
   return (
     <div className="banner">
       {/* 이동슬라이드 */}
-      <ul className="slider">
-        {makeList(selData)}</ul>
+      <ul className="slider">{makeList(selData)}</ul>
 
       {/* 이동버튼 + 슬라이드 블릿 : 
       슬라이드 2개 이상일때 보임 */}
-{
-  // 조건식 && 코드 : 조건식이 true일때 코드출력
-  // 배열.length 는 배열개수
-  selData.length > 1 &&
-  <>
-      {/* 양쪽이동버튼 */}
-      <button className="abtn lb">＜</button>
-      <button className="abtn rb">＞</button>
-      {/* 블릿 인디케이터 - 선택데이터의 개수만큼 만들기 */}
-      <ol className="indic">
-        {
-          selData.map((v,i)=>
-          <li className={i==0?"on":""} key={i}></li>)
-        }
-      </ol>
-  </>
+      {
+        // 조건식 && 코드 : 조건식이 true일때 코드출력
+        // 배열.length 는 배열개수
+        selData.length > 1 && (
+          <>
+            {/* 양쪽이동버튼 */}
+            <button className="abtn lb">＜</button>
+            <button className="abtn rb">＞</button>
+            {/* 블릿 인디케이터 - 선택데이터의 개수만큼 만들기 */}
+            <ol className="indic">
+              {selData.map((v, i) => (
+                <li className={i == 0 ? "on" : ""} key={i}></li>
+              ))}
+            </ol>
+          </>
+        )
       }
     </div>
   );
