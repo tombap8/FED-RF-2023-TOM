b8f39f869116e0f569c642461a8954a980ee23fd
Tom B. Parker <tombap8@hotmail.com>
2023-11-10 16:20:18 +0900

    ss

---
+++ 004.React학습/react-app/tom-app/my-first-react-app/src/dc/layout/Layout.jsx
@@ -0,0 +1,14 @@
+// DC.com 레이아웃 컴포넌트
+
+import { FooterArea } from "./FooterArea";
+import { MainArea } from "./MainArea";
+import { TopArea } from "./TopArea";
+
+export function Layout(){
+    <>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </>
+
+} /////////// Layout 컴포넌트 ///////////
--- 004.React학습/react-app/tom-app/my-first-react-app/src/index.js
+++ 004.React학습/react-app/tom-app/my-first-react-app/src/index.js
@@ -1,32 +1,65 @@
 // index.js는 public/index.html 페이지에 적용되는 컴포넌트다!->루트 컴포넌트
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 // css 도 불러온다!
 import "./css/index.css";
-import { TopArea } from "./dc/layout/TopArea";
-import { MainArea } from "./dc/layout/MainArea";
-import { FooterArea } from "./dc/layout/FooterArea";
+import { Layout } from "./dc/layout/Layout";
+import { Main } from "./dc/contents/Main";
+import { Character } from "./dc/contents/Charactor";
+import { Comics } from "./dc/contents/Comics";
+import { Movies } from "./dc/contents/Movies";
+import { Games } from "./dc/contents/Games";
+import { News } from "./dc/contents/News";
+import { Video } from "./dc/contents/Video";
+import { SwiperApp } from "./dc/plugin/SwiperApp";
 
-function App() {
-  // 상단메뉴 클릭시 메인컨텐츠 변경을 위해
-  // 후크관리변수를 생성한다!
-  const [menu, setMenu] = useState("main");
+/********************************************* 
+    [ 리액트 라우터 ]
+    -> 컴포넌트를 연결하여 특정 이벤트에 모듈을
+    변경해주는 중계역할을 함
+    1. <BrowserRouter> - 라우터 Root
+    2. <Routes> - 개별 라우터를 묶어주는 역할
+    3. <Route> - 개별 라우터
+        (속성)
+            (1) path : 경로를 지정함
+                    (Link의 to속성 경로와 일치함!)
+            (2) element : 연결할 컴포넌트 지정
 
-  // 메뉴 업데이트 함수
-  const chgMenu = (txt) => {
-    console.log("메뉴 업데이트!", txt);
-    // 상태관리변수 변경
-    setMenu(txt);
-  }; ////////// chgMenu 함수 //////
+        (하위 라우트 만들기)
+            <Route path="/">
+                <Route />
+                <Route />
+                <Route />
+            </Route>
+    4. 라우터를 구성하는 컴포넌트는 자체적으로
+    내보내기 셋팅을 해야한다!
+    -> export default 라우터 컴포넌트
+*********************************************/
 
+// 라우터구성 컴포넌트 : 스스로 내보내기 셋팅 필수!
+// 레이아웃 컴포넌트를 라우터에 입혀서 화면에
+// 출력해야하기 때문에 스스로 내보내기를 셋팅해야하는 것!
+export default function App() {
   return (
-    <>
-      <TopArea chgFn={chgMenu} />
-      <MainArea cat={menu} />
-      <FooterArea />
-    </>
+    <BrowserRouter>
+      <Routes>
+        {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정! */}
+        <Route path="/" element={<Layout />}>
+          {/* 하위 라우트 셋팅 */}
+          <Route index element={<Main />} />
+          <Route path="character" element={<Character />} />
+          <Route path="comics" element={<Comics />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="games" element={<Games />} />
+          <Route path="news" element={<News />} />
+          <Route path="video" element={<Video />} />
+          <Route path="swiperapp" element={<SwiperApp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
-}
+} ///////////// App 컴포넌트 ///////////////////
 
 // 컴포넌트 출력 //////////
 // 먼저 root객체만들고
