3159fd72d23f7a13cc898d8666c46ee84a7f0d6f
Tom B. Parker <tombap8@hotmail.com>
2023-11-13 12:13:16 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -1,17 +1,77 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// index.js는 public/index.html 페이지에 적용되는 컴포넌트다!->루트 컴포넌트
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+// css 도 불러온다!
+import "./css/index.css";
+import { Layout } from "./dc/layout/Layout";
+import { Main } from "./dc/contents/Main";
+import { Character } from "./dc/contents/Charactor";
+import { Comics } from "./dc/contents/Comics";
+import { Movies } from "./dc/contents/Movies";
+import { Games } from "./dc/contents/Games";
+import { News } from "./dc/contents/News";
+import { Video } from "./dc/contents/Video";
+import { SwiperApp } from "./dc/plugin/SwiperApp";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+/********************************************* 
+    [ 리액트 라우터 ]
+    -> 컴포넌트를 연결하여 특정 이벤트에 모듈을
+    변경해주는 중계역할을 함
+    1. <BrowserRouter> - 라우터 Root
+    2. <Routes> - 개별 라우터를 묶어주는 역할
+    3. <Route> - 개별 라우터
+        (속성)
+            (1) path : 경로를 지정함
+                    (Link의 to속성 경로와 일치함!)
+            (2) element : 연결할 컴포넌트 지정
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+        (하위 라우트 만들기)
+            <Route path="/">
+                <Route />
+                <Route />
+                <Route />
+            </Route>
+    4. 라우터를 구성하는 컴포넌트는 자체적으로
+    내보내기 셋팅을 해야한다!
+    -> export default 라우터 컴포넌트
+
+    [ 리액트 라우터 흐름 ]
+    1. index.js 에 라우터 중앙 셋팅
+    2. Layout.jsx 레이아웃 컴포넌트를 루트로 선택
+    3. 상단영역 GNB에 <Link to> 셋팅
+    4. 메인영역에 <Outlet /> 셋팅
+    
+*********************************************/
+
+// 라우터구성 컴포넌트 : 스스로 내보내기 셋팅 필수!
+// 레이아웃 컴포넌트를 라우터에 입혀서 화면에
+// 출력해야하기 때문에 스스로 내보내기를 셋팅해야하는 것!
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정! */}
+        <Route path="/" element={<Layout />}>
+          {/* 하위 라우트 셋팅 
+          - path대신 index만 쓰면 첫페이지로 로딩함! 
+          -> path는 Layout의 Link to="/" 에 해당하는 셋팅*/}
+          <Route index element={<Main />} />
+          <Route path="character" element={<Character />} />
+          <Route path="comics" element={<Comics />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="games" element={<Games />} />
+          <Route path="news" element={<News />} />
+          <Route path="video" element={<Video />} />
+          <Route path="swiperapp" element={<SwiperApp />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+} ///////////// App 컴포넌트 ///////////////////
+
+// 컴포넌트 출력 //////////
+// 먼저 root객체만들고
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+// render메서드로 출력
+root.render(<App />);
