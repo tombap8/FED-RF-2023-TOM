8e1721bcf0bdfaeeae49ea98b6276da6be25d26a
Tom B. Parker <tombap8@hotmail.com>
2023-11-14 11:52:36 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -7,7 +7,14 @@ export function Main(){
     return(
         <>
             <h1 style={{textAlign:'center'}}>메인 페이지</h1>
-            <Banner category="main" />
+            <Banner 
+                category={
+                "main"+Math.ceil(Math.random()*3)
+                } />
+            {/* 1~3 중 임의의 난수를 발생함 
+            Math.ceil(Math.random()*3)
+            결과적으로 
+            'main1'/'main2'/'main3'중 하나를 불러온다! */}
         </>
     )
 
