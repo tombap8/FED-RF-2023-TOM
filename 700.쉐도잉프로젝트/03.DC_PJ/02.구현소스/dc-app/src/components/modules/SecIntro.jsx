16cd776262702e621a4eb3b979fdc10b7db9a929
Tom B. Parker <tombap8@hotmail.com>
2023-11-15 12:18:45 +0900

    ss

---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -0,0 +1,9 @@
+// DC.com 섹션소개 컴포넌트 : SecIntro
+
+export function SecIntro(){
+
+    return(
+        <></>
+    );
+
+} ////////////// SecIntro 컴포넌트 ///////////
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,12 +1,13 @@
 // 메인페이지 메인컨텐츠
 
 import { Banner } from "../modules/Banner";
+import { SecIntro } from "../modules/SecIntro";
 
 export function Main(){
     // cat 속성으로 메뉴분류 전달
     return(
-        <>
-            <h1 style={{textAlign:'center'}}>메인 페이지</h1>
+        <> 
+            {/* 1. 배너 컴포넌트 */}           
             <Banner 
                 category={
                 "main"+Math.ceil(Math.random()*3)
@@ -15,6 +16,9 @@ export function Main(){
             Math.ceil(Math.random()*3)
             결과적으로 
             'main1'/'main2'/'main3'중 하나를 불러온다! */}
+
+            {/* 2. 섹션소개 컴포넌트 */}
+            <SecIntro />
         </>
     )
 
