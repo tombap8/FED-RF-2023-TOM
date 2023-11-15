b012eff929eb0a278859d45ffe82729a261a1470
Tom B. Parker <tombap8@hotmail.com>
2023-11-15 16:22:34 +0900

    Update MainCont.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -24,12 +24,19 @@ export function MainCont(){
         // 드래그 기능넣기
         // 대상: .slide
         const slide = $('.slide');
+        // 커버
+        const cover = $('.cover')
+        // console.log(cover);
 
         // 드래그 기능넣기
         slide.draggable({axis:'x'});
 
         // 드래그가 끝났을때 슬라이드 위치
         slide.on('dragstop',()=>{
+
+            // 광드래그 막기 커버
+            cover.show();
+
             // 비교를 위한 윈도우 가로값
             let winW = $(window).width();
             // 현재 슬라이드 left값
@@ -37,10 +44,13 @@ export function MainCont(){
             // 이동차이수 = 슬라이드위치값(양수) - 윈도우가로값
             let diff = Math.abs(pos) - winW;
             // 결과해석: 양수->왼쪽으로 이동/음수->오른쪽으로 이동
+
+            // 기준값 : 화면크기를 기준한 부분
+            let gap = winW/10;
             console.log('드래그 멈춤!',pos,winW,diff);
 
             // 왼쪽으로 이동하기
-            if(diff>50){
+            if(diff>gap){
                 slide.animate({left:'-200%'},
                 800,
                 "easeOutQuint",
@@ -48,22 +58,30 @@ export function MainCont(){
                     // 맨앞li 맨뒤로 이동
                     slide.append(slide.find('li').first())
                     // left값 -100%(처음값)
-                    .css({left:"-100%"})
+                    .css({left:"-100%"});
+                    // 커버제거
+                    cover.hide();
                 })
             }
             // 오른쪽으로 이동하기
-            else if(diff<-50){
+            else if(diff<-gap){
                 slide.animate({left:'0'},800,"easeOutQuint",
                 ()=>{
                     // 맨뒤li 맨앞으로 이동
                     slide.prepend(slide.find('li').last())
                     // left값 -100%(처음값)
                     .css({left:"-100%"})
+                    // 커버제거
+                    cover.hide();
                 })
             }
             // 제자리로 ////
             else{
-                slide.animate({left:'-100%'},300,"easeOutQuint")
+                slide.animate({left:'-100%'},300,
+                "easeOutQuint",
+                ()=>{
+                // 커버제거
+                cover.hide()})
             }
 
 
