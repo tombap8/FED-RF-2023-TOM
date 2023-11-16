bb64d92203a2667e0bb871df4c378a7770ca4995
Tom B. Parker <tombap8@hotmail.com>
2023-11-16 13:00:36 +0900

    Update VidIntro.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -5,7 +5,6 @@ import { vidIntroData } from "../data/vid_intro";
 
 // 비디오소개 CSS 불러오기
 import "../../css/vid_intro.css";
-
 /* 구조정의: 
 Root> section.vidbox > div 
 > video box(>video+img) 
@@ -17,6 +16,19 @@ export function VidIntro(props) {
 
     // 선택데이터 : 페이지 분류명으로 선택
     const selData = vidIntroData[props.cat];
+
+    // 링크생성함수 : desc 데이터 / sum 데이터에서 처리
+    const linkCode = (data) => {
+        return(
+            <>
+                {data.split('*')[0]}
+                <a href={selData.link[1]} target='_blank'>
+                    {selData.link[0]}
+                </a>
+                {data.split('*')[1]}
+            </>
+        );
+    };///////// linkCode함수 ////////////
 
   return (
     /* props.cls로 전달되는 값이 'on'/'off'임 */
@@ -32,10 +44,24 @@ export function VidIntro(props) {
           <h3>{selData.stit}</h3>
           {/* 큰제목 */}
           <h2>{selData.btit}</h2>
-          {/* 요약소개 */}
-          <p>{selData.sum}</p>
-          {/* 설명(링크포함) */}
-          <p>{selData.desc}</p>
+          {/* 요약소개(링크포함) : sum */}
+          <p>
+            {/* 특수문자(*)여부에 따라 처리
+            indexOf(문자열) -> 없으면 -1리턴 */}
+            {
+                selData.sum.indexOf('*') == -1 ?
+                selData.sum : linkCode(selData.sum)                
+            }
+          </p>
+          {/* 설명(링크포함) : desc */}
+          <p>
+            {/* 특수문자(*)여부에 따라 처리
+            indexOf(문자열) -> 없으면 -1리턴 */}
+            {
+                selData.desc.indexOf('*') == -1 ?
+                selData.desc : linkCode(selData.desc)                
+            }
+          </p>
         </div>
       </div>
     </section>
