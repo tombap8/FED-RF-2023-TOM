ff377b670fb2a169e43af37a095ed5bb2bef2bbe
Tom B. Parker <tombap8@hotmail.com>
2023-11-16 11:52:30 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidIntro.jsx
@@ -1,29 +1,30 @@
-// DC PJ 비디오소개 컴포넌트 
+// DC PJ 비디오소개 컴포넌트
 
-// 구조정의: 
-// Root > video box(>video+img) 
-// + title box(>heading+p tag)
+/* 구조정의: 
+Root> section.vidbox > div 
+> video box(>video+img) 
++ title box(>heading+p tag) */
 
-export function VidIntro(){
-
-    return(
-        <section className="vidbox">
-            {/* 1. 비디오파트 */}
-            <div className="vb1">
-                <iframe src="" title=""></iframe>
-            </div>
-            {/* 2. 타이틀/설명파트 */}
-            <div className="vb2">
-                {/* 작은제목 */}
-                <h3></h3>
-                {/* 큰제목 */}
-                <h2></h2>
-                {/* 요약소개 */}
-                <p></p>
-                {/* 설명(링크포함) */}
-                <p></p>
-            </div>
-        </section>
-    );
-
+export function VidIntro() {
+  return (
+    <section className="vidbox">
+      <div>
+        {/* 1. 비디오파트 */}
+        <div className="vb1">
+          <iframe src="" title=""></iframe>
+        </div>
+        {/* 2. 타이틀/설명파트 */}
+        <div className="vb2">
+          {/* 작은제목 */}
+          <h3></h3>
+          {/* 큰제목 */}
+          <h2></h2>
+          {/* 요약소개 */}
+          <p></p>
+          {/* 설명(링크포함) */}
+          <p></p>
+        </div>
+      </div>
+    </section>
+  );
 } //////////// VidIntro 컴포넌트 ///////////
