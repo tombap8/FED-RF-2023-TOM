18dc9596696e7436361210e730640837b57025fb
Tom B. Parker <tombap8@hotmail.com>
2023-11-17 15:20:33 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
@@ -4,17 +4,17 @@ export const fsData = {
     men:{
         isrc:"./images/men02.png",
         ialt:"공유다!",
-        tit:`MEN'S<br />ESSENTIALS`
+        tit:`MEN'S^ESSENTIALS`
     },
     women:{
         isrc:"./images/women01.png",
         ialt:"여자",
-        tit:`WOMEN'S<br />ESSENTIALS`
+        tit:`WOMEN'S^ESSENTIALS`
     },
     // 스타일 데이터는 *로 구분하여 2개를 사용함!
     style:{
         isrc:"./images/style02.png*./images/style03.png",
         ialt:"여자스타일*공유스타일",
-        tit:`MEN'S<br />STYLE*WOMEN'S<br />STYLE`
+        tit:`MEN'S^STYLE*WOMEN'S^STYLE`
     },
 }; //////////// fsData ////////////
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/FashionIntro.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/FashionIntro.jsx
@@ -1,21 +1,28 @@
 // Pilot PJ - 패션인트로 컴포넌트 ////////
 
+// 패션 인트로 데이터 불러오기
+import { fsData } from "../data/fashion_intro";
+
 // 패션 인트로 CSS 불러오기
 import "../css/fashion_intro.css";
 
 export function FashionIntro(props) {
   // props.cat - 카테고리 분류명
 
+  // 선택데이터
+  const selData = fsData[props.cat];
+
   return (
     <div id={props.cat} className="fs-page">
       <ul className="pgc">
         <li className="imgc">
-          <img src="" alt="" />
+          <img src={selData.isrc} alt={selData.ialt} />
         </li>
         <li className="txtc">
           <h2>
             <a href="#">
-              
+            {selData.tit.split('^')[0]} <br />
+            {selData.tit.split('^')[1]}
             </a>
           </h2>
         </li>
