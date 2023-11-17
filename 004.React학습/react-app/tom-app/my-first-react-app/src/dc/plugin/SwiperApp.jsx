ff95a7555d9bd8c96874a617dbc7879b7d529f36
Tom B. Parker <tombap8@hotmail.com>
2023-11-17 11:40:31 +0900

    Update SwiperApp.jsx

--- 004.React학습/react-app/tom-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
+++ 004.React학습/react-app/tom-app/my-first-react-app/src/dc/plugin/SwiperApp.jsx
@@ -29,6 +29,7 @@ export function SwiperApp() {
     "dcm12",
   ];
 
+
   // 상태관리변수 : 멈춤상태 / 플레이상태
   const [sts,setSts] = useState(1);
 
@@ -44,15 +45,30 @@ export function SwiperApp() {
     myRef.current.swiper.autoplay.stop():
     myRef.current.swiper.autoplay.start();
 
-    // 상태값 업데이트
+    // 상태값 업데이트 -> 컴포넌트 리랜더링!!!
     sts? setSts(0) : setSts(1);
     
+    console.log('리랜더링:',myFirst,mySecond.current);
+    
   }; ///////// stopPlay함수 /////////
+
+  // 일반 변수와 useRef사용변수의 차이 //////////
+  let myFirst = '스와이퍼';
+  const mySecond = useRef('갤러리');
+
+  // 변수값 업데이트 함수
+  const myFn = () => {
+    myFirst = 'Swiper';
+    mySecond.current = 'Gallery';
+    console.log('함수호출:',myFirst,mySecond.current);
+  }; ////////// myFn함수 ////////////
 
 
   // 리턴코드 ///////////////////
   return (
     <>
+      <h1 style={{padding:'20px'}}>
+        {myFirst+' : '+mySecond.current}</h1>
       <Swiper
       /* ref 속성에 useRef 할당변수를 넣어서 외부에 연결함 */
         ref={myRef}
@@ -93,6 +109,9 @@ export function SwiperApp() {
       title={sts?"멈추기":"자동넘기기"}
       onClick={stopPlay}
       >{sts?'▣':'▶'}</button>
+
+      {/* useRef 테스트버튼 */}
+      <button onClick={myFn}>useRef 테스트</button>
     </>
   );
 } /////////// SwiperApp 컴포넌트 ///////////
