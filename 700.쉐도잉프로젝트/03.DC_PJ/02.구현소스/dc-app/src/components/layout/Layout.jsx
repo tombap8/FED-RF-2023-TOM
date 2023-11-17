326145ec28f4ba6ef106d4996de0e341a7dda42b
Tom B. Parker <tombap8@hotmail.com>
2023-11-17 12:21:27 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -1,16 +1,27 @@
-// DC.com 레이아웃 컴포넌트
+// DC.com 레이아웃 컴포넌트 : 실제적인 최상위 컴포넌트임!
 
 import { FooterArea } from "./FooterArea";
 import { MainArea } from "./MainArea";
 import { TopArea } from "./TopArea";
 
+// Context API 불러오기
+import { dcCon } from "../modules/dcContext";
+import { useNavigate } from "react-router-dom";
+
 export function Layout(){
+
+  // 라우터 이동객체설정
+  const goNav = useNavigate();
+
+  // 라우터 이동함수
+  const chgPage = (txt) => goNav(txt); 
+
     return(
-    <>
+    <dcCon.Provider value={{chgPage}}>
       <TopArea />
       <MainArea />
       <FooterArea />
-    </>
+    </dcCon.Provider>
 
     );
 
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SecIntro.jsx
@@ -6,22 +6,29 @@ import { secIntroData } from "../data/sec_intro";
 // 섹션소개모듈용 CSS 불러오기
 import "../../css/sec_intro.css"
 
+// 컨텍스트 API를 사용하는 컴포넌트 파일에서 불러옴!
+import { dcCon } from "./dcContext";
+import { useContext } from "react";
+
 // 링크 이동을 위한 라우터 불러오기
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 // 구조정의:
 // Root > section > img Box + title Box + button Box
 
 export function SecIntro(){
 
+    // 컨텍스트 API 사용하기
+    const myCon = useContext(dcCon);
+
     // 선택데이터
     const selData = secIntroData;
 
-    // 라우터 이동객체설정
-    const goNav = useNavigate();
+    // 라우터 이동객체설정 -> 컨텍스트 API사용!
+    // const goNav = useNavigate(); -> 여기서 사용안함
 
-    // 라우터 이동함수
-    const chgPage = (txt) => goNav(txt); 
+    // 라우터 이동함수 -> 컨텍스트 API사용!
+    // const chgPage = (txt) => goNav(txt);  -> 여기서 사용안함
 
 
 
@@ -44,7 +51,10 @@ export function SecIntro(){
                     </div>
                     {/* 3. 버튼박스 */}
                     <div className="btnbx">
-                        <button onClick={()=>chgPage(v.link)}>
+                        <button 
+                        onClick={()=>
+                        // 컨텍스트 API 함수호출!
+                        myCon.chgPage(v.link)}>
                             {v.btn.toUpperCase()}
                         </button>
                     </div>
