2283a198cbdc59df096ff999e3fcbfa4f572a11c
Tom B. Parker <tombap8@hotmail.com>
2023-11-17 12:46:23 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/Layout.jsx
@@ -7,22 +7,32 @@ import { TopArea } from "./TopArea";
 // Context API 불러오기
 import { dcCon } from "../modules/dcContext";
 import { useNavigate } from "react-router-dom";
+import { useLayoutEffect } from "react";
 
-export function Layout(){
+export function Layout() {
+
+  // 랜더링 후(화면보이기전) 실행구역 //////////
+  useLayoutEffect(()=>{
+    // 페이지 이동시 스크롤위치 상단이동
+    window.scrollTo(0,0);
+  }); /////////// useEffect ///////////
 
   // 라우터 이동객체설정
   const goNav = useNavigate();
 
   // 라우터 이동함수
-  const chgPage = (txt) => goNav(txt); 
+  const chgPage = (txt) => goNav(txt);
 
-    return(
-    <dcCon.Provider value={{chgPage}}>
+  /********************************** 
+   [컨텍스트 API 공유값 설정]
+   1. chgPage 함수 : 라우터 이동기능   
+   **********************************/
+  // 리턴코드 ////////////////////////
+  return (
+    <dcCon.Provider value={{ chgPage }}>
       <TopArea />
       <MainArea />
       <FooterArea />
     </dcCon.Provider>
-
-    );
-
+  );
 } /////////// Layout 컴포넌트 ///////////
