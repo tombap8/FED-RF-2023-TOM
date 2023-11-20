afbc63a18c3202441f247d210ed0ba3926f59e6d
Tom B. Parker <tombap8@hotmail.com>
2023-11-20 15:12:21 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,9 +1,13 @@
 // 메인 페이지 JS - index.js
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
 import { TopArea } from './layout/TopArea';
 import { MainArea } from './layout/MainArea';
 import { FooterArea } from './layout/FooterArea';
+
+// 제이쿼리 
+import $ from 'jquery';
+import 'jquery-ui-dist/jquery-ui';
 
 
 // 페이지 공통 CSS
@@ -20,6 +24,25 @@ function App(){
     setPgName(txt);
   }; ///////// chgPgName 함수 //////
 
+  // 랜더링 후 실행구역 ////////////
+  useEffect(()=>{
+    $('.gnb li').click(function(){
+      // 순번변수
+      let idx = $(this).index();
+      console.log('나야나~!',idx);
+
+      // 페이지 이동
+      $("html,body").animate({
+        scrollTop:
+        ($(window).height()*idx)+"px"
+      },800,"easeInOutQuint")
+    })
+
+
+  }); ////////// useEffect //////////////
+
+
+  // 리턴코드 //////////////////////////
   return(
       <>
         <TopArea cat={pgName} />        
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/MainCont.jsx
@@ -14,19 +14,22 @@ export function MainCont() {
   useEffect(() => {
     // 랜더링 후 한번만 적용!
     // console.log("랜더링OK!");
-    
+
     //자동스크롤 호출
     autoScroll();
 
     //드래그배너 호출
     dragBanner();
-    
   }, []); /////// useEffect ///////////
 
   return (
     <>
       {/* 1. 배너 페이지 */}
-      <section id="ban" className="page" style={{ background: "lightblue" }}>
+      <section
+        id="ban"
+        className="page none-sel"
+        style={{ background: "lightblue" }}
+      >
         <Banner />
       </section>
 
@@ -39,14 +42,37 @@ export function MainCont() {
       <section className="page">
         <FashionIntro cat="women" />
       </section>
-      
+
       {/* 4. 스타일패션 페이지 */}
       <section className="page">
         <FashionIntro cat="style" />
       </section>
 
-      {/* 5. 하단 공통영역 */}
-      <section className="page" style={{ background: "lightpink" }}></section>
+      {/* 메인에만 나오는 사이드 인디케이터 */}
+      <nav className="indic">
+        <ul>
+          <li className="on">
+            <a href="#ban">
+              <span>배너</span>
+            </a>
+          </li>
+          <li>
+            <a href="#men">
+              <span>남성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#women">
+              <span>여성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#style">
+              <span>스타일</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 } //////// MainCont 컴포넌트 ///////
