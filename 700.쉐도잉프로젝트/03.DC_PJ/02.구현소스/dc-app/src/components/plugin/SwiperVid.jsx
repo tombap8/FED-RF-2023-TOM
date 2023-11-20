663d8c973675203677c8a8f358b86f74fd408201
Tom B. Parker <tombap8@hotmail.com>
2023-11-20 11:21:07 +0900

    Update SwiperVid.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperVid.jsx
@@ -1,56 +1,60 @@
-// 스와이퍼 플러그인 컴포넌트
+// 비디오스와이프 하위 스와이퍼 플러그인 컴포넌트
 
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+/* 제이쿼리 넣기 */
+import $ from "jquery";
 
 // Import Swiper styles
 import "swiper/css";
-import "swiper/css/pagination";
+// 양쪽 이동버튼만 필요함!
 import 'swiper/css/navigation';
 
-import "./css/swiper.css";
+/* 폰트어썸 임포트 */
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+// 스와이퍼 CSS
+import "./css/swiper_vid.css";
 
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다
 // (여기서는 페이지네이션,네비게이션,자동넘김)
-import { Pagination,Navigation,Autoplay } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 
 export function SwiperApp() {
-  // 불러올 이미지 리스트
-  const imgArr = [
-    "dcm28",
-    "dcm29",
-    "dcm30",
-    "dcm31",
-    "dcm32",
-    "dcm10",
-    "dcm11",
-    "dcm12",
-  ];
+  
 
   return (
     <>
       <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}  
-        autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-        }}      
-        loop={true}
+        slidesPerView={4}
+        spaceBetween={20}
         navigation={true}
         /* 사용할 모듈을 여기에 적용시킨다 */
-        modules={[Pagination,Navigation,Autoplay]}
+        modules={[Navigation]}
         className="mySwiper"
       >
         {
-            imgArr.map((v,i)=>
+            selData.map((v,i)=>
             <SwiperSlide key={i}>
-                <img src={"./images/"+v+".jpg"} alt="list image" />
+                <section className="sw-inbox">
+                  {/* 동영상이미지박스 */}
+                  <div className="vid-img">
+                    <img src={v.src} alt={v.tit} />
+                    {/* 폰트어썸 아이콘 */}
+                    <FontAwesomeIcon
+                      icon={faPlayCircle}
+                      style={{
+                        poisition:'absolute',
+                        bottom: '55%',
+                        left:'10%',
+                        color:'#fff',
+                        fontSize:'50px'
+                      }} />
+                  </div>
+                </section>
             </SwiperSlide>)
         }        
        
