661a2a69edd5695d852ca45c3a76f0a0b48c5272
Tom B. Parker <tombap8@hotmail.com>
2023-11-21 15:11:31 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/VidSwipe.jsx
@@ -15,7 +15,13 @@ import "../../css/vid_swipe.css"
 import { SwiperVid } from "../plugin/SwiperVid";
 
 export function VidSwipe(props){
-    // props.tit - 비디오 스와이프 타이틀
+    // props.cat - 카테고리명
+
+    // 비디오스와이프 카테고리별 타이틀
+    const catTit = {
+        main:"LATEST TRAILERS, CLIPS & MORE",
+        movies:"TRAILERS, CLIPS AND MORE",
+    }
 
     // 리턴코드 //////////////////
     return(
@@ -23,9 +29,12 @@ export function VidSwipe(props){
         {/* 모듈코드 */}
         <section className="vid-swbox">
             {/* 1. 모듈타이틀 */}
-            <h2 className="tit">{props.tit}</h2>
-            {/* 2. 스와이퍼 컴포넌트 : SwiperVid */}
-            <SwiperVid />
+            <h2 className="tit">
+                {catTit[props.cat]}
+            </h2>
+            {/* 2. 스와이퍼 컴포넌트 : SwiperVid
+            -> 전달속성 cat은 데이터선택을 위한값 */}
+            <SwiperVid cat={props.cat} />
 
             {/* 3. 비디오 재생창 */}
             <section className="vid-bx">
--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -28,7 +28,7 @@ export function Main(){
             <VidIntro cat="main" cls="off" />
 
             {/* 4. 비디오스와이프 컴포넌트 */}
-            <VidSwipe tit="LATEST TRAILERS, CLIPS & MORE" />
+            <VidSwipe cat="main" />
 
             {/* 5. 캐릭터 리스트 컴포넌트 */}
             <CatList />
