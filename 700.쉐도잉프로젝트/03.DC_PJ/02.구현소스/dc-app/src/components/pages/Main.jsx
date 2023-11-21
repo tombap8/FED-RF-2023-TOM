5ed6f77f4feefcf1e7d7462c4f6c746f45996125
Tom B. Parker <tombap8@hotmail.com>
2023-11-21 13:20:48 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Main.jsx
@@ -1,6 +1,7 @@
 // 메인페이지 메인컨텐츠
 
 import { Banner } from "../modules/Banner";
+import { CatList } from "../modules/CatList";
 import { SecIntro } from "../modules/SecIntro";
 import { VidIntro } from "../modules/VidIntro";
 import { VidSwipe } from "../modules/VidSwipe";
@@ -28,6 +29,9 @@ export function Main(){
 
             {/* 4. 비디오스와이프 컴포넌트 */}
             <VidSwipe tit="LATEST TRAILERS, CLIPS & MORE" />
+
+            {/* 5. 캐리터 리스트 컴포넌트 */}
+            <CatList />
         </>
     )
 
---
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -0,0 +1,69 @@
+/* DC PJ 캐릭터 리스트용 스와이퍼 컴포넌트 */
+
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Link } from "react-router-dom";
+
+// 캐릭터 리스트 데이터 가져오기
+import { catListData } from "../data/swiper_cat";
+
+
+// Import Swiper styles
+import "swiper/css";
+import 'swiper/css/navigation';
+
+import "./css/swiper_cat.css";
+
+// import required modules
+// 사용할 스와이퍼 모듈을 불러온다
+// (여기서는 페이지네이션,네비게이션,자동넘김)
+import { Navigation } from "swiper/modules";
+
+export function SwiperCat() {
+
+  // 선택데이터
+  const selData = catListData;
+  
+
+  return (
+    <>
+      <Swiper
+        // slidesPerView={7}
+        spaceBetween={20}
+        navigation={true}
+        /* 사용할 모듈을 여기에 적용시킨다 */
+        modules={[Navigation]}
+        // 스와이퍼 사이즈별 슬라이드수 변경!
+        breakpoints={{
+          200: {
+              slidesPerView: 3,
+          },
+          700: {
+              slidesPerView: 4,
+          },
+          1000: {
+              slidesPerView: 5,
+          },
+          1200: {
+              slidesPerView: 7,
+          },
+        }}
+        className="mySwiper2"
+      >
+        {
+            selData.map((v,i)=>
+            /* idx 고유번호가 7번이하만 출력 */
+            Number(v.idx) <= 7 && (
+              <SwiperSlide key={i}>
+                  
+              </SwiperSlide>
+            )
+
+            )
+        }        
+       
+      </Swiper>
+    </>
+  );
+} /////////// SwiperApp 컴포넌트 ///////////
