717d36a5ee6d5d2d75bd2bc2343a1d30d5c3c416
Tom B. Parker <tombap8@hotmail.com>
2023-11-21 14:46:54 +0900

    Update SwiperCat.jsx

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/plugin/SwiperCat.jsx
@@ -8,10 +8,9 @@ import { Link } from "react-router-dom";
 // 캐릭터 리스트 데이터 가져오기
 import { catListData } from "../data/swiper_cat";
 
-
 // Import Swiper styles
 import "swiper/css";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 
 import "./css/swiper_cat.css";
 
@@ -21,10 +20,8 @@ import "./css/swiper_cat.css";
 import { Navigation } from "swiper/modules";
 
 export function SwiperCat() {
-
   // 선택데이터
   const selData = catListData;
-  
 
   return (
     <>
@@ -37,32 +34,39 @@ export function SwiperCat() {
         // 스와이퍼 사이즈별 슬라이드수 변경!
         breakpoints={{
           200: {
-              slidesPerView: 3,
+            slidesPerView: 3,
           },
           700: {
-              slidesPerView: 4,
+            slidesPerView: 4,
           },
           1000: {
-              slidesPerView: 5,
+            slidesPerView: 5,
           },
           1200: {
-              slidesPerView: 7,
+            slidesPerView: 7,
           },
         }}
         className="mySwiper2"
       >
-        {
-            selData.map((v,i)=>
+        {selData.map(
+          (v, i) =>
             /* idx 고유번호가 7번이하만 출력 */
-            Number(v.idx) <= 7 && (
-              <SwiperSlide key={i}>
-                  
-              </SwiperSlide>
-            )
-
-            )
-        }        
-       
+            Number(v.idx) <= 7 && 
+            <SwiperSlide key={i}>
+              <Link to="/detail">
+                <section className="sw-inbox2">
+                  {/* 캐릭터이미지영역 */}
+                  <div className="cat-img2">
+                    <img src={v.tmsrc} alt={v.cname} />
+                  </div>
+                  {/* 캐릭터타이틀영역 */}
+                  <div className="cat-tit2">
+                    <h3>{v.cname}</h3>
+                  </div>
+                </section>
+              </Link>
+            </SwiperSlide>
+        )}
       </Swiper>
     </>
   );
