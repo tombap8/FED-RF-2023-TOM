e400a29493f9fb0793554adef143a3fcc7477837
Tom B. Parker <tombap8@hotmail.com>
2023-11-24 14:55:16 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/FooterArea.jsx
@@ -1,11 +1,35 @@
 // 하단영역 컴포넌트
 
+import { Logo } from "../modules/Logo";
+
+// 하단링크정보 데이터 불러오기
+import { bmData } from "../data/bmenu";
+
 export function FooterArea(){
     return(
         <footer className="info">
-            All Site Content © &amp; TM DC, unless otherwise noted here.
-            <br />
-            All rights reserved.
+            <ul>
+                <li>
+                    <Logo logoStyle="bottom" />
+                </li>
+                <li>
+                    {/* 하단링크박스 */}
+                    <ol className="bmenu">
+                        {
+                            bmData.map((v,i)=>
+                                <li key={i}>
+                                    <a href={v.link} target="_blank">
+                                        {v.txt.toUpperCase()}
+                                    </a>
+                                </li>
+                            )
+                        }
+                    </ol>
+                </li>
+                <li>
+                © & ™ DC. ALL RIGHTS RESERVED
+                </li>
+            </ul>
         </footer>
     )
 
