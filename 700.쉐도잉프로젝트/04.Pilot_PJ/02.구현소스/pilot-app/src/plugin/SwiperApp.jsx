1dd897e329b97dbe118152d1a4bfd303af6e2147
Tom B. Parker <tombap8@hotmail.com>
2023-11-24 15:16:36 +0900

    Update SwiperApp.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/plugin/SwiperApp.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/plugin/SwiperApp.jsx
@@ -6,57 +6,56 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-import 'swiper/css/navigation';
+import "swiper/css/navigation";
 
 import "./css/swiper.css";
 
 // import required modules
 // 사용할 스와이퍼 모듈을 불러온다
 // (여기서는 페이지네이션,네비게이션,자동넘김)
-import { Pagination,Navigation,Autoplay } from "swiper/modules"
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import { useRef, useState } from "react";
 
 export function SwiperApp(props) {
-
   // 리스트만들기 함수 ////
-  const makeList = (num) => { // num
-    let temp = []; 
-    for(let x=0;x<num;x++){
-      temp[x] = 
-      <SwiperSlide>
-          <img src={"./images/sub/"+props.cat+"/banner/ban"+(x+1)+".png"} />
-      </SwiperSlide>
-    }; /////////// for /////////////
+  const makeList = (num) => {
+    // num
+    let temp = [];
+    for (let x = 0; x < num; x++) {
+      temp[x] = (
+        <SwiperSlide>
+          <img
+            src={"./images/sub/" + props.cat + "/banner/ban" + (x + 1) + ".png"}
+          />
+        </SwiperSlide>
+      );
+    } /////////// for /////////////
 
     // 배열을 리턴
     return temp;
-
-  } ///////////// makeList 함수 //////////
-
+  }; ///////////// makeList 함수 //////////
 
   // 리턴코드 ///////////////////
   return (
     <>
       <Swiper
-      /* ref 속성에 useRef 할당변수를 넣어서 외부에 연결함 */
+        /* ref 속성에 useRef 할당변수를 넣어서 외부에 연결함 */
         slidesPerView={1}
         spaceBetween={0}
         pagination={{
           clickable: true,
-        }}  
+        }}
         autoplay={{
-            delay: 3000,
-            disableOnInteraction: false,
-        }}      
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
         loop={true}
         navigation={true}
         /* 사용할 모듈을 여기에 적용시킨다 */
-        modules={[Pagination,Navigation,Autoplay]}
+        modules={[Pagination, Navigation, Autoplay]}
         className="mySwiper"
-      >     
-            
-            {makeList(props.cat=='style'?5:3)}
-            
+      >
+        {makeList(props.cat == "style" ? 5 : 3)}
       </Swiper>
     </>
   );
