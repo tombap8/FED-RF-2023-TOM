eb4b8033368855e1b44818f9cb846d555a71c65c
Tom B. Parker <tombap8@hotmail.com>
2023-11-27 11:47:01 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SchCatList.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/modules/SchCatList.jsx
@@ -5,37 +5,57 @@ import { Link } from "react-router-dom";
 import { catListData } from "../data/swiper_cat";
 
 // 캐릭터 검색 리스트 CSS 가져오기
-import "../../css/search_cat_list.css"
+import "../../css/search_cat_list.css";
 
 export function SchCatList(props) {
-    // props.word - 데이터 검색값
+  // props.word - 데이터 검색값
+
+  // 전달된 검색어 소문자변환
+  let kword = props.word.toLowerCase();
 
   // 선택데이터
-  const selData = catListData.filter(v=>{
-    if(v.cname.toLowerCase().indexOf(props.word) !== -1) return true;
+  const selData = catListData.filter((v) => {
+    if (v.cname.toLowerCase().indexOf(kword) !== -1) return true;
   });
 
-  console.log(selData);
+  // 선택데이터 개수
+  const selCnt = selData.length;
+
+  console.log(selData, selCnt);
 
   return (
     <>
-      <ul className="clist">
-        {selData.map((v, i) => (
-          <li key={i}>
-            <Link
-              to="/detail"
-              state={{
-                cname: v.cname,
-                cdesc: v.cdesc,
-                facts: v.facts,
-              }}
-            >
-              <img src={v.tmsrc} alt={v.cname} />
-              <h3>{v.cname}</h3>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      {
+        // 데이터 개수가 0이 아닐때
+        selCnt != 0 && (
+          <ul className="clist">
+            {selData.map((v, i) => (
+              <li key={i}>
+                <Link
+                  to="/detail"
+                  state={{
+                    cname: v.cname,
+                    cdesc: v.cdesc,
+                    facts: v.facts,
+                  }}
+                >
+                  <img src={v.tmsrc} alt={v.cname} />
+                  <h3>{v.cname}</h3>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )
+      }
+      {
+        // 선택데이터가 0개이면
+        selCnt == 0 && (
+          <h2 style={{textAlign:'center'}}>
+            Sorry, we don't have any matches for that. But there's plenty more
+            to see on DC!
+          </h2>
+        )
+      }
     </>
   );
 } ///////// SchCatList /////////////////
