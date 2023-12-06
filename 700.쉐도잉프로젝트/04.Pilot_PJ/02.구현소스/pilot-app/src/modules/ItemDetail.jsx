ff65683c8623c29d3e597620183090e9e2355070
Tom B. Parker <tombap8@hotmail.com>
2023-12-06 10:20:28 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/modules/ItemDetail.jsx
@@ -1,8 +1,10 @@
 // 상품상세보기 컴포넌트
 
-// 신상품 데이터 가져오기
 import { useEffect } from "react";
+// 신상품 데이터 가져오기
+import gdata from "../data/glist-items";
 import { sinsangData } from "../data/sinsang";
+
 
 import $ from 'jquery';
 import { CartList } from "./CartList";
@@ -16,7 +18,15 @@ export function ItemDetail({cat,goods}) {
   // -> 개별상품 배열이 된다!
   // [상품명,상품코드,가격]
   const selData = sinsangData[cat][goods].split('^');
-  console.log('선택데이터:',selData);
+  // console.log('선택데이터:',selData);
+
+  const selD = gdata.filter(v=>{
+    // 조건: 분류와 상품분류코드가 일치하는 하나
+    if(v.cat===cat && v.ginfo[0]===goods)
+     return true;
+  })
+
+  console.log('새로선택:',selD);
 
 
   // 닫기 함수 ////
