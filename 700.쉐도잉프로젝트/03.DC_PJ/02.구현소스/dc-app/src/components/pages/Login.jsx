3a4e58a93eef5ef1b6904c8ee2c4085ee794be7a
Tom B. Parker <tombap8@hotmail.com>
2023-12-11 12:13:56 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Login.jsx
@@ -1,10 +1,193 @@
 // 로그인 페이지 컴포넌트 - Login.jsx
 
-export function Login(){
-    return(
-        <>
-        <h1>나는 로그인이야~!</h1>
-        </>
-    )
+// 디자인은 회원가입과 동일!
+import { useState } from "react";
+import "../../css/member.css";
 
+// 제이쿼리
+import $ from "jquery";
+
+export function Login() {
+  // [ 상태관리변수 ] /////////
+  // [1] 입력요소 상태변수 /////////
+  // 1. 아이디변수
+  const [userId, setUserId] = useState("");
+  // 2. 비밀번호변수
+  const [pwd, setPwd] = useState("");
+  // [2] 에러상태관리 변수
+  // -> 에러상태값 초기값은 에러아님(false)
+  // 1. 아이디변수
+  const [userIdError, setUserIdError] = useState(false);
+  // 2. 비밀번호변수
+  const [pwdError, setPwdError] = useState(false);
+  // [ 아이디관련 메시지 프리셋 ] ////
+  const msgId = [
+    "User ID must contain a minimum of 5 characters",
+    "This ID is already in use!",
+    "That's a great ID!",
+  ];
+  // [ 기타 메시지 프리셋 ]
+  const msgEtc = {
+    // 비밀번호
+    pwd: "5 to 15 digits in the form of special characters, characters, and numbers",
+    // 비밀번호확인
+    confPwd: "Password verification does not match",
+    // 필수입력
+    req: "This is a required entry",
+    // 이메일
+    email: "Please enter a valid email format",
+  }; ///// msgEtc ///////
+  // [3] 에러메시지 상태변수
+  const [idMsg, setIdMsg] = useState(msgId[0]);
+
+  // [ 유효성 검사 함수 ] ///////
+  // 1. 아이디 유효성 검사 ///////////
+  const changeUserId = (e) => {
+    // 1-1. 빈값 체크
+    if (e.target.value !== "") setUserIdError(false);
+    else {
+      // 빈값일 경우
+      // 메시지 띄우기 : "This is a required entry"
+      setIdMsg(msgEtc.req);
+      // 에러상태값 변경하기
+      setUserIdError(true);
+    } ///// else ////
+    // 1-2. 입력값 반영하기
+    setUserId(e.target.value);
+  }; ///////// changeUserId 함수 //////////
+
+  // 2. 비밀번호 유효성 검사 ///////////
+  const changePwd = (e) => {
+    // 2-1. 빈값 체크
+    if (e.target.value !== "") setPwdError(false);
+    else {
+      // 빈값일 경우
+      // 메시지 띄우기 : "This is a required entry"
+      setIdMsg(msgEtc.req);
+      // 에러상태값 변경하기
+      setPwdError(true);
+    } ///// else ////
+    // 2-2. 입력값 반영하기
+    setPwd(e.target.value);
+  }; ///////// changePwd 함수 //////////
+
+  // 3. 전체 유효성검사 함수 ////////////
+  const totalValid = () => {
+    // 3-1. 모든 상태변수에 빈값일때 에러상태값 업데이트!
+    if (!userId) setUserIdError(true);
+    if (!pwd) setPwdError(true);
+
+    // 3-2. 통과시 true, 불통과시 false 리턴처리
+    // 통과조건 : 빈값아님 + 에러후크변수가 모두 false
+    if (userId && pwd) return true;
+    // 하나라도 false이면 false를 리턴함!
+    else return false;
+  }; /////////// totalValid 함수 ////////
+
+  // 4. 서브밋 기능함수 ///////////////
+  const onSubmit = (e) => {
+    // 4-1.기본 서브밋 이동막기
+    e.preventDefault();
+
+    // 4-2. 유효성검사 전체 통과시 ////
+    if (totalValid) {
+      console.log("통과!");
+    } ///// if ///////
+    // 4-3. 유효성검사 불통과시 /////
+    else {
+      console.log("실패!");
+    } ////// else ///////
+  }; //////////// onSubmit 함수 ///////////
+
+  // 리턴코드 ///////////////////////////
+  return (
+    <div className="outbx">
+      {/* 모듈코드 */}
+      <section className="membx" style={{ minHeight: "300px" }}>
+        <h2>LOG IN</h2>
+        <form method="post" action="process.php">
+          <ul>
+            <li>
+              {/* 1.아이디 */}
+              <label>ID : </label>
+              <input
+                type="text"
+                maxLength="20"
+                placeholder="Please enter your ID"
+                value={userId}
+                onChange={changeUserId}
+              />
+              {
+                // 에러일 경우 메시지출력
+                // 조건문 && 요소
+                userIdError && (
+                  <div className="msg">
+                    <small
+                      style={{
+                        color: "red",
+                        fontSize: "10px",
+                      }}
+                    >
+                      {idMsg}
+                    </small>
+                  </div>
+                )
+              }
+
+              {
+                // 통과시 메시지출력
+                // 조건문 && 요소
+                // 조건추가 : userId가 입력전일때 안보임처리
+                // userId가 입력전엔 false로 리턴됨!
+                !userIdError && userId && (
+                  <div className="msg">
+                    <small
+                      style={{
+                        color: "green",
+                        fontSize: "10px",
+                      }}
+                    >
+                      {msgId[2]}
+                    </small>
+                  </div>
+                )
+              }
+            </li>
+            <li>
+              {/* 2.비밀번호 */}
+              <label>Password : </label>
+              <input
+                type="password"
+                maxLength="20"
+                placeholder="Please enter your Password"
+                value={pwd}
+                onChange={changePwd}
+              />
+              {
+                // 에러시 메시지출력
+                pwdError && (
+                  <div className="msg">
+                    <small
+                      style={{
+                        color: "red",
+                        fontSize: "10px",
+                      }}
+                    >
+                      {msgEtc.pwd}
+                    </small>
+                  </div>
+                )
+              }
+            </li>
+            {/* 3.서브밋버튼 */}
+            <li>
+              <button className="sbtn" onClick={onSubmit}>
+                Submit
+              </button>
+            </li>
+          </ul>
+        </form>
+      </section>
+    </div>
+  );
 }
