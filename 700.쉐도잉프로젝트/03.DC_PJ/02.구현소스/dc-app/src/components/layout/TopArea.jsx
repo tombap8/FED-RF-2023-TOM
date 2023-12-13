32c35b683b635eb6779425a8bcaab10f98cdef10
Tom B. Parker <tombap8@hotmail.com>
2023-12-13 11:01:04 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -13,7 +13,7 @@ import $ from 'jquery';
 // 폰트어썸 불러오기
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { memo, useContext, useState } from "react";
+import { memo, useContext, useEffect, useState } from "react";
 
 /******************************************************* 
   [ 리액트 라우터와 연결하여 사용되는 라우터 컴포넌트 ]
@@ -66,6 +66,8 @@ export const TopArea = memo(
     // console.log(e.target);
     // 엔터키는 'Enter'문자열을 리턴함!
     if(e.key === 'Enter'){ 
+      // 모바일에서 열린 메뉴창 닫기
+      $(".top-area").removeClass('on');
       // 입력창의 입력값 읽어오기 : val() 사용!
       let txt = $(e.target).val().trim();
       console.log(txt);
@@ -88,6 +90,19 @@ export const TopArea = memo(
 
   // 햄버거용 함수 : 전체메뉴 보이기
   const showMenu = () => $(".top-area").toggleClass('on');
+
+  // 랜더링후 실행구역 ///////////////
+  useEffect(()=>{
+
+    // GNB a요소 클릭시 전체메뉴 닫기
+    // 대상: .gnb a[href!='#'] 
+    // -> href가 '#'이 아닌 gnb 하위 모든 a요소
+    // -> != 은 제이쿼리전용!
+    $(".gnb a[href!='#']").on('click',()=>{
+      $(".top-area").removeClass('on');
+    }); /////////// click //////////
+
+  }); ///////// useEffect /////////
 
 
   // 리턴코드 ///////////////////////////
@@ -181,9 +196,9 @@ export const TopArea = memo(
               </>
             }
           </ul>
+        </nav>
           {/* 모바일용 햄버거 버튼 */}
           <button className="hambtn" onClick={showMenu}></button>
-        </nav>
       </header>
     </>
   );
