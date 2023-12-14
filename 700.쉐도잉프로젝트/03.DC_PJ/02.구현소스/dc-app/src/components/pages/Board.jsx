b8deeec3bedf7cd55aaeed6b5d6ff5b67e8583ac
Tom B. Parker <tombap8@hotmail.com>
2023-12-14 13:14:51 +0900

    ss

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/pages/Board.jsx
@@ -23,6 +23,8 @@ if (localStorage.getItem("bdata"))
   orgData = JSON.parse(localStorage.getItem("bdata"));
 // 로컬스 없으면 제이슨 데이터 넣기
 else orgData = baseData;
+// else orgData = [];
+
 
 // console.log(org);
 
@@ -32,7 +34,7 @@ export function Board() {
   // 1. 페이지 단위수 : 한 페이지 당 레코드수
   const pgBlock = 7;
   // 2. 전체 레코드수 : 배열데이터 총개수
-  const totNum = baseData.length;
+  const totNum = orgData.length;
   console.log("페이지단위수:", pgBlock, "\n전체 레코드수:", totNum);
 
   // [ 상태관리 변수 셋팅 ] ////////
@@ -56,26 +58,35 @@ export function Board() {
     console.log("다시바인딩!", pgNum);
     // 데이터 선별하기
     const tempData = [];
-    
+
     // 시작값 : (페이지번호-1)*블록단위수
     let initNum = (pgNum - 1) * pgBlock;
     // 한계값 : 블록단위수*페이지번호
     let limitNum = pgBlock * pgNum;
-    
+
     // 블록단위가 7일 경우 첫페이지는 0~7, 7~14,...
-    console.log("시작값:", initNum, 
-    "\n한계값:", limitNum);
+    console.log("시작값:", initNum, "\n한계값:", limitNum);
 
     // 데이터 선별용 for문 : 원본데이터(orgData)로부터 생성
     for (let i = initNum; i < limitNum; i++) {
       // 마지막 페이지 한계수체크
-      if(i>=totNum) break; 
+      if (i >= totNum) break;
       // 코드 푸시
       tempData.push(orgData[i]);
     } ///// for /////
 
     console.log("결과셋:", tempData);
 
+    // 데이터가 없는 경우 출력 ///
+    if (orgData.length === 0) {
+      return (
+        <tr>
+          <td colSpan="5">There is no data.</td>
+        </tr>
+      );
+    } ////// if /////////
+
+    // if문에 들어가지 않으면 여기를 리턴함!
     return tempData.map((v, i) => (
       <tr key={i}>
         {/* 1. 일련번호 */}
@@ -94,10 +105,6 @@ export function Board() {
         <td>{v.cnt}</td>
       </tr>
     ));
-
-    // <tr>
-    //   <td colSpan="5">There is no data.</td>
-    // </tr>
   }; /////////// bindList 함수 ////////////
 
   /************************************* 
@@ -134,9 +141,14 @@ export function Board() {
     for (let i = 0; i < limit; i++) {
       pgCode[i] = (
         <Fragment key={i}>
-          <a href="#" onClick={chgList}>
-            {i + 1}
-          </a>{" "}
+          {pgNum - 1 === i ? (
+            <b>{i + 1}</b>
+          ) : (
+            <a href="#" onClick={chgList}>
+              {i + 1}
+            </a>
+          )}
+
           {i < limit - 1 ? " | " : ""}
         </Fragment>
       );
