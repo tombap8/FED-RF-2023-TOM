45b1e3485b37eacc84b2e90ecbe688051eeac949
Tom B. Parker <tombap8@hotmail.com>
2023-12-22 16:22:09 +0900

    Update GList.jsx

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/GList.jsx
@@ -22,10 +22,12 @@ export function GList() {
 
   // 리랜더링을 위한 상태변수 : 무조건 리랜더링설정목적
   const [force, setForce] = useState(null);
+  // 데이터 상태관리 변수
+  const [currData, setCurrData] = useState(gdata);
 
   // 리스트 만들기 함수 ////////
   const makeList = () =>
-    gdata.map((v, i) => (
+    currData.map((v, i) => (
       <div key={i}>
         <a
           href="#"
@@ -86,9 +88,68 @@ export function GList() {
     const chked = e.target.checked;
     console.log('아이디:',cid,chked);
 
-    // 4.체크박스 체크개수세기 : 1개초과시 배열합치기!
+    // 3.체크박스 체크개수세기 : 1개초과시 배열합치기!
     let num = $('.chkbx:checked').length;
     console.log('체크개수:',num);
+
+    // 4. 기존 입력 데이터 가져오기
+    // 현재 상태관리 데이터 배열값(변경되는 데이터)
+    let temp = currData;
+
+    // 결과집합배열변수 : 최종결과배열
+    let lastList = [];
+
+    // 5. 체크박스 체크유무에 따른 분기
+    // (1) 체크박스가 true일대 해당 검색어로 검색하기
+    if(chked){
+      // 현재데이터 변수에 담기(원본데이터로 부터!)
+      const nowList = gdata.filter(v=>{
+        if(v.cat===cid) return true;
+      }) /////////// filter //////////
+
+      // 체크개수가 1초과일때 배열합치기
+      if(num>1){ // 스프레드 연산자(...)사용!
+        lastList = [...temp,...nowList];
+      } //// if /////
+      else{ // 하나일때
+        lastList = nowList;
+      }
+
+    } /////////// if /////////
+    // (2) 체크박스가 false일때 데이터 지우기
+    else{
+      console.log('지울데이터:',cid);
+      // for문을 돌면서 배열데이터중 해당값을 지운다!
+      for(let i=0; i<temp.length;i++){
+        // -> 삭제대상:
+        // 데이터중 alignment 항목값이 아이디명과 같은것
+        if(temp[i].alignment==cid){
+          // 해당항목 지우기
+          // 배열지우기 메서드 : splice(순번,개수)
+          temp.splice(i,1);
+          // 주의! 배열을 지우면 전체개수가 1씩줄어든다!
+          // 반드시 줄임처리할것!
+          i--;
+
+          // 참고테스트 : 배열삭제 delete는 무엇인가?
+          // delete배열지우기는 값만지우고 주소는 남는다!
+          // 지운후 값은 undefined로 남아진다!
+          // delete temp[i];
+          // -> 리스트처리시 에러발생함!
+          // 여기서는 splice를 반드시 사용할것!
+
+        } //////// if ///////
+      } ///////// for ////////
+
+      console.log('삭제처리된배열:',temp);
+
+      // 결과처리하기 : 삭제처리된 temp를 결과에 넣기!
+      lastList = temp;
+
+    } /////////// else ///////////
+
+    // 6. 검색결과 리스트 업데이트 하기
+    setCurrData(lastList);
 
   }; ////////////// changeList 함수 ///////////
 
