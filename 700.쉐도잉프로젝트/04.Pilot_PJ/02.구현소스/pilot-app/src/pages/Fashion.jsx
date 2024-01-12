8567491b5582b5b40ae36313847bc96526cfcc2c
Tom B. Parker <tombap8@hotmail.com>
2024-01-12 11:06:09 +0900

    ss

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/pages/Fashion.jsx
@@ -1,6 +1,12 @@
 // 공통패션 서브페이지 컨텐츠 컴포넌트
 
-import { useContext, useEffect, useLayoutEffect, useReducer, useState } from "react";
+import {
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useReducer,
+  useState,
+} from "react";
 
 // 공통 서브 CSS 불러오기
 import "../css/fashion.css";
@@ -17,12 +23,16 @@ import { ItemDetail } from "../modules/ItemDetail";
 // 부드러운 스크롤 JS
 import { scrolled, setPos } from "../func/smoothScroll24";
 
+// 리액트용 패럴랙스 - 설치 : npm i react-parallax
+import { Parallax } from "react-parallax";
+// 설명 : https://www.npmjs.com/package/react-parallax
+
 export function Fashion(props) {
   // 컨텍스트 API 사용!
   const myCon = useContext(pCon);
 
   // props.cat - 서브 카테고리명
-  console.log('패션페이지cat:',props.cat);
+  console.log("패션페이지cat:", props.cat);
 
   useEffect(() => {
     // [부드러운 스크롤 함수 이벤트 설정하기]
@@ -69,21 +79,18 @@ export function Fashion(props) {
       // 부드러운 스크롤 위치값 초기화!!!
       setPos(0);
     }; /////// 소멸자 ////////////////
-
   }, []); ///////// useEffect ///////////
 
   // props.cat 카테고리가 변경될때만 맨위로 값 변경!
-  useLayoutEffect(()=>{
-    console.log('같으면 실행안함?');
+  useLayoutEffect(() => {
+    console.log("같으면 실행안함?");
     // 부드러운 스크롤 위치값
     setPos(0);
     // 윈도우 실제로 상단이동
     window.scrollTo(0, 0);
     // 열렸을 수 있는 상세페이지 닫기
-    $('.bgbx').hide();
-  },[props.cat]);
-
-  
+    $(".bgbx").hide();
+  }, [props.cat]);
 
   // 후크 상태변수
   const [item, setItem] = useState("m1");
@@ -114,8 +121,17 @@ export function Fashion(props) {
       <div className="bgbx">
         <ItemDetail goods={item} cat={props.cat} />
       </div>
-      {/* 3. 패럴랙스 영역 */}
-      <section id="c2" className="cont c2 men"></section>
+      {/* 3. 패럴랙스 영역 : 리액트용 패럴랙스 적용 */}
+      <section id="c2" className="cont">
+        <Parallax
+          className="c2"
+          // 패럴랙스할 배경이미지 설정속성 bgImage
+          bgImage={"./images/sub/" + props.cat + "/02.special.png"}
+          // 패럴랙스 이동정도 조정속성 strength
+          // 수치범위 :  -500 ~ 1000 -> 높은 숫자는 반대방향
+          strength={200}
+        ></Parallax>
+      </section>
       {/* 4. 단일상품영역 */}
       <section id="c3" className="cont c3"></section>
       {/* 5. 스타일상품영역 */}
