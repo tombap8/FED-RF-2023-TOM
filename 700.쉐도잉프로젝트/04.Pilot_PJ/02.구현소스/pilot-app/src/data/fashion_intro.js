1a6bfbd90a321a033423e46e055dd53c07b37929
Tom B. Parker <tombap8@hotmail.com>
2024-01-12 11:43:06 +0900

    Update fashion_intro.js

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/data/fashion_intro.js
@@ -24,16 +24,16 @@ export const fsData = {
   sub: {
     men: [
       {
-        isrc: "./images/men02.png",
-        ialt: "공유다!",
-        tit: ["MEN'S", "ESSENTIALS"],
+        isrc: "./images/sub/men/03.disc.png",
+        ialt: "공유가 해변에 있다!",
+        tit: ["MEN'S", "BEACH STYLE"],
       },
       {
-        isrc: ["./images/style02.png", "./images/style03.png"],
-        ialt: ["여자스타일", "공유스타일"],
+        isrc: ["./images/sub/men/04.disc.png", "../images/sub/men/05.disc.png"],
+        ialt: ["남자스타일1", "남자스타일2"],
         tit: [
-          ["MEN'S", "STYLE"],
-          ["WOMEN'S", "STYLE"],
+          ["MEN'S", "SPORT STYLE"],
+          ["MEN'S", "LIFE STYLE"],
         ],
       },
     ],
