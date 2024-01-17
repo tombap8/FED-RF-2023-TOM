bba0a5115913a69d8b67a798a9c805ae582a0a64
Tom B. Parker <tombap8@hotmail.com>
2024-01-17 12:52:05 +0900

    ss

--- 006.SVG학습/js/movie_intro.js
+++ 006.SVG학습/js/movie_intro.js
@@ -28,3 +28,48 @@ setTimeout(() => {
 setTimeout(() => {
     logoAni.addClass('anion');
 }, 12000);
+
+
+/* 오디오 컨트롤 하기 */
+// 대상:  .play-box
+$('.play-box').hover(
+    function(){ // over
+        console.log('오버야!');
+        // 오버시 진한 이미지로 변경
+
+        // 기존값 읽어오기
+        let csrc = $('img',this).attr('src');
+
+        // 경로에서 '.png' 를 '-1.png' 로 변경
+        $('img',this)
+        .attr('src',csrc.replace('.png','-1.png'));
+    },
+    function(){ // out
+        console.log('아웃이야!');
+        
+        // 기존값 읽어오기
+        let csrc = $('img',this).attr('src');
+
+        // 경로에서 '.png' 를 '-1.png' 로 변경
+        $('img',this)
+        .attr('src',csrc.replace('-1.png','.png'));
+
+    }) /////////// hover /////////
+    // 클릭시 이미지 변경하기 + 오디오 재생/멈춤하기
+    .on('click',function(){
+        
+        // 오디오요소 : 제이쿼리는 get(0)으로 요소를 선택
+        const myAudio = $('.my-audio').get(0);
+        
+        // 현재 오디오 멈춤여부 알아오기
+        let isPaused = myAudio.paused;
+        console.log('멈췄니?',isPaused);
+
+        // paused 는 오디오/비디오 멈춤여부를 리턴함
+        // 멈춤상태명 true / 재생상태면 false
+
+
+
+
+
+    }); //////// click ///////////
