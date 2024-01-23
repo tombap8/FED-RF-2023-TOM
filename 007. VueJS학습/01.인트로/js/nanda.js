fc13afad4c1a0299645f4fa9cc847dc846272e09
Tom B. Parker <tombap8@hotmail.com>
2024-01-23 16:19:18 +0900

    ss

--- 007. VueJS학습/01.인트로/js/nanda.js
+++ 007. VueJS학습/01.인트로/js/nanda.js
@@ -7,5 +7,14 @@ const vm = new Vue({
     // 대상은 꼭 아이디요소일 필요는 없다!
     // 클래스나 일반요소를 쓰면 첫번째 만나는요소에
     // 적용된다!
-    
-});
+
+    // 2. 데이터 설정하기
+    data: {
+        // 2-1. 제목 데이터
+        bigTit : 'Style NANDA',
+        // 2-2. 로고 태그정보
+        logo: `<img src="./images/logo_3ce.png" alt="nanda logo">`,
+
+    }, //// data /////
+
+}); //////// Vue 인스턴스 설정 /////////
