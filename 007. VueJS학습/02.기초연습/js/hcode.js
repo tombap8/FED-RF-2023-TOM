584415010dd561e0ed91c28897327f0411aacabe
Tom B. Parker <tombap8@hotmail.com>
2024-01-24 17:46:42 +0900

    ss

---
+++ 007. VueJS학습/02.기초연습/js/hcode.js
@@ -0,0 +1,20 @@
+// 쇼핑몰 갤러리 템플릿 데이터 JS - hcode.js
+
+const hcode = {
+  // 1. 타이틀
+  tit: `
+        <strong>
+            <span>👩‍🦰다이아나 쇼핑몰💕</span><br>
+            👗Diana Shopping Mall🥻
+        </strong>
+    `,
+  // 2. 리스트
+  list: `
+`,
+  // 3. 큰이미지
+  big: `
+`,
+}; ////////////// hcode객체 //////////////
+
+// 객체 내보내기
+export default hcode;
---
+++ 007. VueJS학습/02.기초연습/js/small.js
@@ -0,0 +1,9 @@
+// 쇼핑몰 갤러리 JS - small.js
+
+// 템플릿 html코드 객체 JS 가져오기
+import hcode from "./hcode.js";
+
+// 뷰JS 인스턴스 생성용 함수!
+const makeVue = (x) => new Vue({ el: x });
+
+// 1. 제목에 넣을 전역컴포넌트 만들기
