3aa1dc7ca411f80ac3c97a65c727ddcfad87c31e
Tom B. Parker <tombap8@hotmail.com>
2024-01-26 17:52:46 +0900

    ss

---
+++ 007. VueJS학습/05.SPA구성연습/city-vue-app/src/components/InfoComp.vue
@@ -0,0 +1,27 @@
+<template>
+    <footer>
+        <address>
+            서울시 강남구 역삼동 119 뷰엑스B
+        </address>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "InfoArea",
+    data() {
+        return {
+            msg: "Welcome to Your Vue.js App",
+        };
+    },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer {
+    border: 1px solid #ccc;
+    text-align: center;
+    padding: 20px;
+}
+</style>
--- 007. VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
+++ 007. VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
@@ -2,14 +2,22 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// 뷰엑스 스토어 JS 불러오기
+import store from './store';
+// import router from './router'
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  store, // 스토어 사용 등록!!!
+  // router, // 라우터 사용 등록!!!
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  // 뷰 인스턴스 생성 직후 호출코드구역
+  created(){
+    // 데이터 초기화 메서드 호출!
+    store.commit('initSet');
+  },
 })
