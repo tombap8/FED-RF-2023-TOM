28e16bed89b3a91615ec8018a8c94fb97743aca4
Tom B. Parker <tombap8@hotmail.com>
2024-01-26 17:17:23 +0900

    ss

--- 007. VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
+++ 007. VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <TopArea />
+    <MainArea />
+    <InfoArea />
   </div>
 </template>
 
 <script>
+import TopArea from "./components/TopComp.vue";
+import MainArea from "./components/MainComp.vue";
+import InfoArea from "./components/InfoComp.vue";
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TopArea,
+    MainArea,
+    InfoArea,
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 500px;
+  margin: 0 auto;
 }
 </style>
